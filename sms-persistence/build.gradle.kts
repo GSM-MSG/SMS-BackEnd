dc055e90962c1379b9a59bc0fd6d58f7626997d0
Huuuunee <s21024@gsm.hs.kr>
2023-04-12 15:38:49 +0900

    :sparkles: 새로운 모듈 생성 및 상위 폴더 gradle 에서 설정

---
+++ sms-persistence/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("plugin.jpa") version PluginVersions.JPA_PLUGIN_VERSION
+    kotlin("plugin.spring") version PluginVersions.SPRING_PLUGIN_VERSION
+}
+
+dependencies {
+    // impl project
+    implementation(project(":sms-core"))
+
+    // database
+    implementation(Dependencies.SPRING_DATA_JPA)
+    runtimeOnly(Dependencies.MYSQL_CONNECTOR)
+    implementation(Dependencies.REDIS)
+    implementation(Dependencies.SPRING_REDIS)
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
