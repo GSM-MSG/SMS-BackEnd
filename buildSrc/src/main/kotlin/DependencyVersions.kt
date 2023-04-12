cba3c4bc433ac3403c65dc2f8285201ed0573d0e
Huuuunee <s21024@gsm.hs.kr>
2023-04-12 15:38:17 +0900

    :sparkles: 버전 관리 및 의존성을 다른 모듈에서 관리합니다

---
+++ buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -0,0 +1,7 @@
+object DependencyVersions {
+    const val JACKSON_VERSION = "2.10.3"
+    const val JWT_VERSION = "0.9.1"
+    const val REDIS_VERSION = "2.7.2"
+    const val MYSQL = "8.0.28"
+    const val MOCKK_VERSION = "1.13.2"
+}
