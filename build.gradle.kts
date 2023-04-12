ad41545075c650bcd49c0e0008d72ed66fc68921
Huuuunee <s21024@gsm.hs.kr>
2023-04-12 15:38:49 +0900

    :sparkles: 새로운 모듈 생성 및 상위 폴더 gradle 에서 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,50 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-	id("org.springframework.boot") version "2.7.10"
-	id("io.spring.dependency-management") version "1.0.15.RELEASE"
 	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
 }
 
-group = "team.msg"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
 }
 
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
+subprojects {
+	apply {
+		plugin("org.jetbrains.kotlin.jvm")
+		version = "1.6.21"
+	}
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
+	dependencies {
+		// kotlin
+		implementation(Dependencies.KOTLIN_REFLECT)
+		implementation(Dependencies.KOTLIN_JDK)
+		implementation(Dependencies.JACKSON)
+
+		// test
+		implementation(Dependencies.SPRING_TEST)
+		implementation(Dependencies.MOCKK)
 	}
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+allprojects {
+	group = "team.msg"
+	version = "0.0.1-SNAPSHOT"
+	java.sourceCompatibility = JavaVersion.VERSION_11
+
+	tasks {
+		compileKotlin {
+			kotlinOptions {
+				freeCompilerArgs = listOf("-Xjsr305=strict")
+				jvmTarget = "11"
+			}
+		}
+		compileJava {
+			sourceCompatibility = JavaVersion.VERSION_11.majorVersion
+		}
+		test {
+			useJUnitPlatform()
+		}
+	}
+	repositories {
+		mavenCentral()
+	}
 }
