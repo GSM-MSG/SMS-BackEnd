bcc10c4fd4ac037233f657eceff4db7bcbbd05b7
Huuuunee <s21024@gsm.hs.kr>
2023-04-15 20:11:31 +0900

    :sparkles: 세부스택 엔티티 생성

---
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/techstack/entity/TechStackJpaEntity.kt
@@ -0,0 +1,15 @@
+package team.msg.sms.persistence.techstack.entity
+
+import team.msg.sms.persistence.BaseIdEntity
+import team.msg.sms.persistence.student.entity.StudentJpaEntity
+import javax.persistence.*
+
+@Entity
+@Table(name = "tech_stack")
+class TechStackJpaEntity(
+    @Column
+    val stack: String,
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id")
+    val student: StudentJpaEntity
+) : BaseIdEntity()
