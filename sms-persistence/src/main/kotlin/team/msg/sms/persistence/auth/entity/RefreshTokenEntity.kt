f659c610ce0b700828d9b0e8c217b569b1196235
전승원 <78971821+louis7308@users.noreply.github.com>
2023-04-20 00:16:42 +0900

    :ambulance: :: user 기본키 타입을 Long -> UUID 로 변경

--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/auth/entity/RefreshTokenEntity.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/auth/entity/RefreshTokenEntity.kt
@@ -3,6 +3,7 @@ package team.msg.sms.persistence.auth.entity
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 import org.springframework.data.redis.core.index.Indexed
+import java.util.UUID
 
 @RedisHash(value = "refresh_token", timeToLive = 60L * 60 * 24 * 7)
 data class RefreshTokenEntity(
@@ -10,5 +11,5 @@ data class RefreshTokenEntity(
     @Indexed
     val token: String,
     @Indexed
-    val userId: Long
+    val userId: UUID
 )
