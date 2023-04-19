ac5e7c13bc4a26c3d65fbefbdecaeb2d58061134
전승원 <78971821+louis7308@users.noreply.github.com>
2023-04-20 00:26:30 +0900

    :sparkles: :: TokenResponse Dto

---
+++ sms-core/src/main/kotlin/team/msg/sms/domain/auth/dto/response/TokenResponse.kt
@@ -0,0 +1,13 @@
+package team.msg.sms.domain.auth.dto.response
+
+import java.time.LocalDateTime
+
+data class TokenResponse(
+    val accessToken: String,
+
+    val accessTokenExp: LocalDateTime,
+
+    val refreshToken: String,
+
+    val refreshTokenExp: LocalDateTime
+)
