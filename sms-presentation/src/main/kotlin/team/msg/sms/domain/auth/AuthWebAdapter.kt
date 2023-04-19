bd80cb55a18fefb965939ce16cb789898636bfcf
전승원 <78971821+louis7308@users.noreply.github.com>
2023-04-20 00:25:38 +0900

    :sparkles: :: SignIn 비즈니스 로직 구현 및 presentation 영역도 구현

---
+++ sms-presentation/src/main/kotlin/team/msg/sms/domain/auth/AuthWebAdapter.kt
@@ -0,0 +1,26 @@
+package team.msg.sms.domain.auth
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import team.msg.sms.domain.auth.dto.SignInRequest
+import team.msg.sms.domain.auth.dto.response.SignInResponse
+import team.msg.sms.domain.auth.usecase.SignInUseCase
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/auth")
+class AuthWebAdapter(
+    private val signInUseCase: SignInUseCase
+) {
+
+    @PostMapping
+    fun signIn(@Valid @RequestBody request: SignInRequest): ResponseEntity<SignInResponse> =
+        signInUseCase.execute(request.toData())
+            .let { ResponseEntity.ok(it) }
+
+}
