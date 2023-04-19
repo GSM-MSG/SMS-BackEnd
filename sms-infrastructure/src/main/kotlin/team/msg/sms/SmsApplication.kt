0d003a637579c3430539a9e1893a65cf2ec99b5a
전승원 <78971821+louis7308@users.noreply.github.com>
2023-04-20 00:22:49 +0900

    :sparkles: Jwt 생성 로직

--- sms-infrastructure/src/main/kotlin/team/msg/sms/SmsApplication.kt
+++ sms-infrastructure/src/main/kotlin/team/msg/sms/SmsApplication.kt
@@ -1,9 +1,11 @@
 package team.msg.sms
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class SmsApplication
 
 fun main(args: Array<String>) {
