35e91ba23563fce86acebdca21e945e029c03ba0
전승원 <78971821+louis7308@users.noreply.github.com>
2023-04-20 00:25:38 +0900

    :sparkles: :: SignIn 비즈니스 로직 구현 및 presentation 영역도 구현

---
+++ sms-core/src/main/kotlin/team/msg/sms/domain/auth/dto/response/SignInResponse.kt
@@ -0,0 +1,14 @@
+package team.msg.sms.domain.auth.dto.response
+
+import java.time.LocalDateTime
+
+data class SignInResponse(
+    val accessToken: String,
+
+    val accessTokenExp: LocalDateTime,
+
+    val refreshToken: String,
+
+    val refreshTokenExp: LocalDateTime
+
+)
