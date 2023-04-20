b272e4977023a58c5e0f92d91d419a797d25eaa1
전승원 <78971821+louis7308@users.noreply.github.com>
2023-04-20 10:23:03 +0900

    :wrench: :: annotation 도 build.src 에서 관리하도록 변경

---
+++ buildSrc/src/main/kotlin/AllOpen.kt
@@ -0,0 +1,7 @@
+object AllOpen {
+    const val USECASE = "team.msg.sms.common.annotation.UseCase"
+
+    const val READ_ONLY_USECASE = "team.msg.sms.common.annotation.ReadOnlyUseCase"
+
+    const val SERVICE = "team.msg.sms.common.annotation.Service"
+}
