a2c86225d85ada6cf82902f0071fff312225099a
전승원 <78971821+louis7308@users.noreply.github.com>
2023-04-20 09:22:07 +0900

    :ambulance: :: Format 잘못 함

--- sms-core/src/main/kotlin/team/msg/sms/domain/auth/dto/response/SignInResponse.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/auth/dto/response/SignInResponse.kt
@@ -1,15 +1,18 @@
 package team.msg.sms.domain.auth.dto.response
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDateTime
 import java.time.ZonedDateTime
 
 data class SignInResponse(
     val accessToken: String,
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     val accessTokenExp: LocalDateTime,
 
     val refreshToken: String,
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     val refreshTokenExp: LocalDateTime
 
 )
