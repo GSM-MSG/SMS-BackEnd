099b434d7501b1bb71b6b6b6fe190b5175f3d6fc
전승원 <78971821+louis7308@users.noreply.github.com>
2023-04-20 09:22:49 +0900

    Update sms-core/src/main/kotlin/team/msg/sms/domain/auth/dto/response/TokenResponse.kt
    Co-authored-by: baegteun <baegteun@gmail.com>

--- sms-core/src/main/kotlin/team/msg/sms/domain/auth/dto/response/TokenResponse.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/auth/dto/response/TokenResponse.kt
@@ -2,6 +2,7 @@ package team.msg.sms.domain.auth.dto.response
 
 import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDateTime
+
 data class TokenResponse(
     val accessToken: String,
 
