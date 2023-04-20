ca35738a9ceadae96404f78e8e41b397f4178899
전승원 <78971821+louis7308@users.noreply.github.com>
2023-04-20 10:59:11 +0900

    :fire: :: Allopen Object 삭제

--- buildSrc/src/main/kotlin/AllOpen.kt
+++
@@ -1,7 +0,0 @@
-object AllOpen {
-        const val USECASE = "team.msg.sms.common.annotation.UseCase"
-
-        const val READ_ONLY_USECASE = "team.msg.sms.common.annotation.ReadOnlyUseCase"
-
-        const val SERVICE = "team.msg.sms.common.annotation.Service"
-}
