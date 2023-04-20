f4a5ca2018ef799cb75c7a08eafc33445465b79f
전승원 <78971821+louis7308@users.noreply.github.com>
2023-04-20 10:52:34 +0900

    :ambulance: :: 이 오류가 맞는지 확인

--- buildSrc/src/main/kotlin/AllOpen.kt
+++ buildSrc/src/main/kotlin/AllOpen.kt
@@ -1,9 +1,7 @@
-class AllOpen {
-    companion object {
+object AllOpen {
         const val USECASE = "team.msg.sms.common.annotation.UseCase"
 
         const val READ_ONLY_USECASE = "team.msg.sms.common.annotation.ReadOnlyUseCase"
 
         const val SERVICE = "team.msg.sms.common.annotation.Service"
-    }
 }
