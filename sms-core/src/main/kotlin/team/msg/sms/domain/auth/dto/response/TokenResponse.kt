5720d039dcf9e5e92beb0e75393c268aa03652d2
전승원 <78971821+louis7308@users.noreply.github.com>
2023-04-20 09:22:07 +0900

    :ambulance: :: Format 잘못 함

--- sms-core/src/main/kotlin/team/msg/sms/domain/auth/dto/response/TokenResponse.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/auth/dto/response/TokenResponse.kt
@@ -5,11 +5,9 @@ import java.time.LocalDateTime
 data class TokenResponse(
     val accessToken: String,
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     val accessTokenExp: LocalDateTime,
 
     val refreshToken: String,
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     val refreshTokenExp: LocalDateTime
 )
