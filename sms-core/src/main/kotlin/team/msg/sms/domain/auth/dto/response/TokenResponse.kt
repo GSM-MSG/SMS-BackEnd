7ab92196b4daced17aa72365539def7c2a70ddfb
전승원 <78971821+louis7308@users.noreply.github.com>
2023-04-20 09:04:27 +0900

    :sparkles: :: 시간 Format 설정 해서 반환

--- sms-core/src/main/kotlin/team/msg/sms/domain/auth/dto/response/TokenResponse.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/auth/dto/response/TokenResponse.kt
@@ -1,13 +1,15 @@
 package team.msg.sms.domain.auth.dto.response
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDateTime
-
 data class TokenResponse(
     val accessToken: String,
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     val accessTokenExp: LocalDateTime,
 
     val refreshToken: String,
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     val refreshTokenExp: LocalDateTime
 )
