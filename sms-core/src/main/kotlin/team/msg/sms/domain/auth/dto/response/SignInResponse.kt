8deab6fcbc228a542ef25fa98fe6870f0318b3dd
전승원 <78971821+louis7308@users.noreply.github.com>
2023-04-25 08:45:57 +0900

    :sparkles: :: DateTimeFormat Jackson 자체 변환시 적용

--- sms-core/src/main/kotlin/team/msg/sms/domain/auth/dto/response/SignInResponse.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/auth/dto/response/SignInResponse.kt
@@ -7,12 +7,10 @@ import java.time.ZonedDateTime
 data class SignInResponse(
     val accessToken: String,
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     val accessTokenExp: LocalDateTime,
 
     val refreshToken: String,
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     val refreshTokenExp: LocalDateTime
 
 )
---
+++ sms-infrastructure/src/main/kotlin/team/msg/sms/global/config/JacksonConfiguration.kt
@@ -0,0 +1,15 @@
+package team.msg.sms.global.config
+
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
+import java.time.format.DateTimeFormatter
+
+@Configuration
+class JacksonConfiguration {
+    @Bean
+    fun jackson2ObjectMapperBuilder(): Jackson2ObjectMapperBuilder =
+        Jackson2ObjectMapperBuilder()
+            .serializers(LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")))
+}
