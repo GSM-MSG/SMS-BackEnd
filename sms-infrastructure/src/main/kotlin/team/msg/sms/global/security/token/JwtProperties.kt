3e7f5975bee67d3fdb55b9f59b89601ca164e415
전승원 <78971821+louis7308@users.noreply.github.com>
2023-04-24 14:18:50 +0900

    Update sms-infrastructure/src/main/kotlin/team/msg/sms/global/security/token/JwtProperties.kt
    Co-authored-by: baegteun <baegteun@gmail.com>

--- sms-infrastructure/src/main/kotlin/team/msg/sms/global/security/token/JwtProperties.kt
+++ sms-infrastructure/src/main/kotlin/team/msg/sms/global/security/token/JwtProperties.kt
@@ -4,6 +4,6 @@ object JwtProperties {
     const val ACCESS = "access"
     const val REFRESH = "refresh"
     const val ROLE = "ROLE"
-    const val PREFIX = "Bearer"
+    const val PREFIX = "Bearer "
     const val HEADER = "Authorization"
 }
