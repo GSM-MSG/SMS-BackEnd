60faa5568e642da20f5839ec190fdf7bc7de8214
huuuunee <s21024@gsm.hs.kr>
2023-04-27 11:24:56 +0900

    :sparkles: 로그아웃 기능 구현

---
+++ sms-core/src/main/kotlin/team/msg/sms/domain/auth/usecase/LogoutUseCase.kt
@@ -0,0 +1,19 @@
+package team.msg.sms.domain.auth.usecase
+
+import team.msg.sms.common.annotation.UseCase
+import team.msg.sms.common.service.SecurityService
+import team.msg.sms.domain.auth.service.AuthService
+import team.msg.sms.domain.user.exception.UserNotFoundException
+
+@UseCase
+class LogoutUseCase(
+    private val securityService: SecurityService,
+    private val authService: AuthService
+) {
+    fun execute(header: String) {
+        val userId = securityService.getCurrentUserId()
+        val token = authService.getRefreshTokenByToken(header)
+        if (token.userId == userId) authService.deleteRefreshToken(token) else throw UserNotFoundException
+    }
+
+}
