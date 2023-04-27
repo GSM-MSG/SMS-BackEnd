6a614e9357bee4bfcb07b2a567d5bf4b2cada785
huuuunee <s21024@gsm.hs.kr>
2023-04-27 11:23:04 +0900

    :sparkles: 요청을 보낸 User Id 를 반환하는 Service 생성

---
+++ sms-core/src/main/kotlin/team/msg/sms/common/service/impl/SecurityServiceImpl.kt
@@ -0,0 +1,14 @@
+package team.msg.sms.common.service.impl
+
+import team.msg.sms.common.annotation.Service
+import team.msg.sms.common.service.SecurityService
+import team.msg.sms.common.spi.SecurityPort
+import java.util.*
+
+@Service
+class SecurityServiceImpl(
+    private val securityPort: SecurityPort
+) : SecurityService {
+    override fun getCurrentUserId(): UUID =
+        securityPort.getCurrentUserId()
+}
