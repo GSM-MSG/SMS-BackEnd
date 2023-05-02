02eb4e1434237cbe237414ddce3509effe3f7f02
huuuunee <s21024@gsm.hs.kr>
2023-05-03 00:09:36 +0900

    :recycle: 어노테이션 추가

--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/languagecertificate/entity/LanguageCertificateJpaEntity.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/languagecertificate/entity/LanguageCertificateJpaEntity.kt
@@ -2,10 +2,14 @@ package team.msg.sms.persistence.languagecertificate.entity
 
 import team.msg.sms.persistence.BaseIdEntity
 import team.msg.sms.persistence.student.entity.StudentJpaEntity
+import javax.persistence.Entity
 import javax.persistence.FetchType
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
+import javax.persistence.Table
 
+@Entity
+@Table(name = "language_certificate")
 class LanguageCertificateJpaEntity(
     val languageCertificateName: String,
 
