eaa9db2d9c02a4248b4bf8cc47aa0ff37536713c
huuuunee <s21024@gsm.hs.kr>
2023-05-03 00:03:14 +0900

    :sparkles: 과 Enum 생성

---
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/model/Department.kt
@@ -0,0 +1,7 @@
+package team.msg.sms.domain.student.model
+
+enum class Department(description: String) {
+    SW_DEVELOPMENT("SW 개발과"),
+    SMART_IOT_DEVELOPMENT("스마트 IOT과"),
+    AI_DEVELOPMENT("인공지능 개발과")
+}
