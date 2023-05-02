f1b2a147da42695f462ff6b618ef46e7736f2b70
huuuunee <s21024@gsm.hs.kr>
2023-05-02 23:34:15 +0900

    :recycle: WorkerType -> FormOfEmployment 로 네이밍 변경

---
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/model/FormOfEmployment.kt
@@ -0,0 +1,8 @@
+package team.msg.sms.domain.student.model
+
+enum class FormOfEmployment(description: String) {
+    FullTime("정규직"),
+    Temporary("비정규직"),
+    Contract("계약직"),
+    Intern("인턴")
+}
