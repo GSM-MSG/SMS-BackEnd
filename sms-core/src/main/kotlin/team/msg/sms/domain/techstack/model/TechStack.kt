c673d6f9fe326a34f2916e14dca4c8f279a9a922
huuuunee <s21024@gsm.hs.kr>
2023-05-09 11:35:06 +0900

    :recycle: TechStack 모듈의 id 타입을 UUID로 변경

--- sms-core/src/main/kotlin/team/msg/sms/domain/techstack/model/TechStack.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/techstack/model/TechStack.kt
@@ -1,10 +1,11 @@
 package team.msg.sms.domain.techstack.model
 
 import team.msg.sms.common.annotation.Aggregate
+import java.util.*
 
 @Aggregate
 data class TechStack(
-    val id: Long,
+    val id: UUID,
     val stack: String,
-    val studentId: Long
+    val studentId: UUID
 )
