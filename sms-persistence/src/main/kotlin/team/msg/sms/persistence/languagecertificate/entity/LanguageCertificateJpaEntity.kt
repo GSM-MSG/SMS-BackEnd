13af4dc1d6c559fdbc0933413239ae0c438ed3f9
huuuunee <s21024@gsm.hs.kr>
2023-05-09 11:33:47 +0900

    :recycle: LanguageCertificate 의 ID 타입을 Long -> UUID 로 변경

--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/languagecertificate/entity/LanguageCertificateJpaEntity.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/languagecertificate/entity/LanguageCertificateJpaEntity.kt
@@ -1,7 +1,9 @@
 package team.msg.sms.persistence.languagecertificate.entity
 
 import team.msg.sms.persistence.BaseIdEntity
+import team.msg.sms.persistence.BaseUuidEntity
 import team.msg.sms.persistence.student.entity.StudentJpaEntity
+import java.util.UUID
 import javax.persistence.Entity
 import javax.persistence.FetchType
 import javax.persistence.JoinColumn
@@ -11,9 +13,11 @@ import javax.persistence.Table
 @Entity
 @Table(name = "language_certificate")
 class LanguageCertificateJpaEntity(
+    override val id: UUID,
+
     val languageCertificateName: String,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "student_id")
     val student: StudentJpaEntity
-) : BaseIdEntity()
+) : BaseUuidEntity(id)
