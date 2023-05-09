a8063b463eecb0d12ec65483cdd9ce381cde0be2
huuuunee <s21024@gsm.hs.kr>
2023-05-09 18:24:36 +0900

    :sparkles: 유저 존재할때 예외 문자 추가

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/exception/error/StudentErrorCode.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/exception/error/StudentErrorCode.kt
@@ -9,7 +9,8 @@ enum class StudentErrorCode(
 ) : ErrorProperty {
 
     STUDENT_NOT_FOUND(ErrorStatus.NOT_FOUND, "학생이 존재하지 않습니다."),
-    STU_NUM_NOR_RIGHT(ErrorStatus.BAD_REQUEST, "올바르지 않은 학번입니다.")
+    STU_NUM_NOR_RIGHT(ErrorStatus.BAD_REQUEST, "올바르지 않은 학번입니다."),
+    STUDENT_ALREADY(ErrorStatus.CONFLICT, "학생 정보가 존재하는 유저입니다")
     ;
 
     override fun status(): Int = status
