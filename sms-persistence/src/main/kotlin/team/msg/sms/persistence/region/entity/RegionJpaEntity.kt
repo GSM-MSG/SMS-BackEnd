b213dc7dc843e9189f2a671e1769aae664ee55c8
huuuunee <s21024@gsm.hs.kr>
2023-05-09 11:33:58 +0900

    :recycle: Region 의 ID 타입을 Long -> UUID 로 변경

--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/region/entity/RegionJpaEntity.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/region/entity/RegionJpaEntity.kt
@@ -1,16 +1,20 @@
 package team.msg.sms.persistence.region.entity
 
 import team.msg.sms.persistence.BaseIdEntity
+import team.msg.sms.persistence.BaseUuidEntity
 import team.msg.sms.persistence.student.entity.StudentJpaEntity
+import java.util.UUID
 import javax.persistence.*
 
 @Entity
 @Table(name = "region")
 class RegionJpaEntity(
+    override val id: UUID,
+
     @Column
     val region: String,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "student_id")
     val student: StudentJpaEntity
-) : BaseIdEntity()
+) : BaseUuidEntity(id)
