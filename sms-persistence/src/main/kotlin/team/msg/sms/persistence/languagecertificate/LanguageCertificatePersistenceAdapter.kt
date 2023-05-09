5b2627068d012c35b6b78c5d26447c7fb5da87ac
huuuunee <s21024@gsm.hs.kr>
2023-05-09 11:57:28 +0900

    :sparkles: LanguageCertificate Repository의 접근할 수 있는 PersistenceAdapter 생성

---
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/languagecertificate/LanguageCertificatePersistenceAdapter.kt
@@ -0,0 +1,21 @@
+package team.msg.sms.persistence.languagecertificate
+
+import org.springframework.stereotype.Component
+import team.msg.sms.domain.languagecertificate.model.LanguageCertificate
+import team.msg.sms.domain.languagecertificate.spi.LanguageCertificatePort
+import team.msg.sms.domain.student.model.Student
+import team.msg.sms.domain.user.model.User
+import team.msg.sms.persistence.languagecertificate.mapper.toDomain
+import team.msg.sms.persistence.languagecertificate.mapper.toEntity
+import team.msg.sms.persistence.languagecertificate.repository.LanguageCertificateJpaRepository
+import team.msg.sms.persistence.student.mapper.toEntity
+import team.msg.sms.persistence.user.mapper.toEntity
+
+@Component
+class LanguageCertificatePersistenceAdapter(
+    private val languageCertificateJpaRepository: LanguageCertificateJpaRepository
+) : LanguageCertificatePort {
+    override fun saveAll(region: List<LanguageCertificate>, student: Student, user: User): List<LanguageCertificate> =
+        languageCertificateJpaRepository.saveAll(region.map { it.toEntity(student.toEntity(user.toEntity())) })
+            .map { it.toDomain() }
+}
