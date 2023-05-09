3d6590b066d71a589104694657fd864c2d932192
huuuunee <s21024@gsm.hs.kr>
2023-05-09 11:56:01 +0900

    :sparkles: LanguageCertificate 도메인화, 엔티티화 시키는 Mapper 생성

---
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/languagecertificate/mapper/LanguageCertificateMapper.kt
@@ -0,0 +1,22 @@
+package team.msg.sms.persistence.languagecertificate.mapper
+
+import team.msg.sms.domain.languagecertificate.model.LanguageCertificate
+import team.msg.sms.persistence.languagecertificate.entity.LanguageCertificateJpaEntity
+import team.msg.sms.persistence.student.entity.StudentJpaEntity
+
+fun LanguageCertificateJpaEntity.toDomain(): LanguageCertificate =
+    LanguageCertificate(
+        id = id,
+        languageCertificateName = languageCertificateName,
+        studentId = student.id
+    )
+
+
+fun LanguageCertificate.toEntity(
+    student: StudentJpaEntity
+): LanguageCertificateJpaEntity =
+    LanguageCertificateJpaEntity(
+        id = id,
+        languageCertificateName = languageCertificateName,
+        student = student
+    )
