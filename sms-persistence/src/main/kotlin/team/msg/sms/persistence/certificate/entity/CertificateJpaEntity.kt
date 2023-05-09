15c10da9fbcd77ac5244a489118225931dc052b7
huuuunee <s21024@gsm.hs.kr>
2023-05-09 11:33:28 +0900

    :recycle: Certificate 의 ID 타입을 Long -> UUID 로 변경

--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/certificate/entity/CertificateJpaEntity.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/certificate/entity/CertificateJpaEntity.kt
@@ -1,16 +1,20 @@
 package team.msg.sms.persistence.certificate.entity
 
 import team.msg.sms.persistence.BaseIdEntity
+import team.msg.sms.persistence.BaseUuidEntity
 import team.msg.sms.persistence.student.entity.StudentJpaEntity
+import java.util.UUID
 import javax.persistence.*
 
 @Entity
 @Table(name = "certificate")
 class CertificateJpaEntity(
+    override val id: UUID,
+
     @Column
     val certificateName: String,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "student_id")
     val student: StudentJpaEntity
-) : BaseIdEntity()
+) : BaseUuidEntity(id)
