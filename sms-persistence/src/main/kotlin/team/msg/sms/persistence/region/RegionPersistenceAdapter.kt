6d248858b78d3ae3aadc894d95c871a375e95fbe
huuuunee <s21024@gsm.hs.kr>
2023-05-09 11:57:35 +0900

    :sparkles: Region Repository의 접근할 수 있는 PersistenceAdapter 생성

---
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/region/RegionPersistenceAdapter.kt
@@ -0,0 +1,21 @@
+package team.msg.sms.persistence.region
+
+import org.springframework.stereotype.Component
+import team.msg.sms.domain.region.model.Region
+import team.msg.sms.domain.region.spi.RegionPort
+import team.msg.sms.domain.student.model.Student
+import team.msg.sms.domain.user.model.User
+import team.msg.sms.persistence.region.mapper.toDomain
+import team.msg.sms.persistence.region.mapper.toEntity
+import team.msg.sms.persistence.region.repository.RegionJpaRepository
+import team.msg.sms.persistence.student.mapper.toEntity
+import team.msg.sms.persistence.user.mapper.toEntity
+
+@Component
+class RegionPersistenceAdapter(
+    private val regionJpaRepository: RegionJpaRepository
+) : RegionPort {
+    override fun saveAll(region: List<Region>, student: Student, user: User): List<Region> =
+        regionJpaRepository.saveAll(region.map { it.toEntity(student.toEntity(user.toEntity())) }).map { it.toDomain() }
+
+}
