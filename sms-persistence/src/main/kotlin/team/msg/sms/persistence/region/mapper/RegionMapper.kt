f4fbd89a3eaadcd54670bde3e672bce83234ad39
huuuunee <s21024@gsm.hs.kr>
2023-05-09 11:55:37 +0900

    :sparkles: Region 도메인화, 엔티티화 시키는 Mapper 생성

---
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/region/mapper/RegionMapper.kt
@@ -0,0 +1,22 @@
+package team.msg.sms.persistence.region.mapper
+
+import team.msg.sms.domain.region.model.Region
+import team.msg.sms.persistence.region.entity.RegionJpaEntity
+import team.msg.sms.persistence.student.entity.StudentJpaEntity
+
+
+fun RegionJpaEntity.toDomain(): Region =
+    Region(
+        id = id,
+        region = region,
+        studentId = student.id
+    )
+
+fun Region.toEntity(
+    student: StudentJpaEntity
+): RegionJpaEntity =
+    RegionJpaEntity(
+        id = id,
+        region = region,
+        student = student
+    )
