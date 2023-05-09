1d955054441fb8b59e7efa25b917668092ee631e
huuuunee <s21024@gsm.hs.kr>
2023-05-09 12:03:05 +0900

    :sparkles: CommandRegionService 인터페이스를 구현하는 구현체 생성

---
+++ sms-core/src/main/kotlin/team/msg/sms/domain/region/service/impl/CommandRegionServiceImpl.kt
@@ -0,0 +1,16 @@
+package team.msg.sms.domain.region.service.impl
+
+import team.msg.sms.common.annotation.Service
+import team.msg.sms.domain.region.model.Region
+import team.msg.sms.domain.region.service.CommandRegionService
+import team.msg.sms.domain.region.spi.RegionPort
+import team.msg.sms.domain.student.model.Student
+import team.msg.sms.domain.user.model.User
+
+@Service
+class CommandRegionServiceImpl(
+    private val regionPort: RegionPort
+) : CommandRegionService {
+    override fun saveAll(region: List<Region>, student: Student, user: User): List<Region> =
+        regionPort.saveAll(region, student, user)
+}
