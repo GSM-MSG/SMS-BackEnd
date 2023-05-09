dfaecf95f0caf5c1f73cba0fdb34738e3c899f5f
huuuunee <s21024@gsm.hs.kr>
2023-05-09 18:25:40 +0900

    :sparkles: CheckStudentService도 StudentService가 상속 추가

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/service/StudentService.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/service/StudentService.kt
@@ -5,5 +5,7 @@ import team.msg.sms.common.annotation.Service
 
 @Service
 class StudentService(
-    commandStudentService: CommandStudentService
-) : CommandStudentService by commandStudentService
+    commandStudentService: CommandStudentService,
+    checkStudentService: CheckStudentService
+) : CommandStudentService by commandStudentService,
+    CheckStudentService by checkStudentService
