73873445b2dd632e771fa89b2eb9aac30b9a9ba9
huuuunee <s21024@gsm.hs.kr>
2023-05-09 11:34:45 +0900

    :recycle: LanguageCertificate 모듈의 id 타입을 UUID로 변경

--- sms-core/src/main/kotlin/team/msg/sms/domain/languagecertificate/model/LanguageCertificate.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/languagecertificate/model/LanguageCertificate.kt
@@ -1,10 +1,11 @@
 package team.msg.sms.domain.languagecertificate.model
 
 import team.msg.sms.common.annotation.Aggregate
+import java.util.*
 
 @Aggregate
 class LanguageCertificate(
-    val id: Long,
+    val id: UUID,
     val languageCertificateName: String,
-    val studentId: Long
+    val studentId: UUID
 )
