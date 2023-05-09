1527cf113329068a9aa4400fb113feb8bc5733f1
huuuunee <s21024@gsm.hs.kr>
2023-05-09 12:05:04 +0900

    :sparkles: LanguageCertificate Service의 구현체 생성

---
+++ sms-core/src/main/kotlin/team/msg/sms/domain/languagecertificate/service/impl/CommandLanguageCertificateServiceImpl.kt
@@ -0,0 +1,21 @@
+package team.msg.sms.domain.languagecertificate.service.impl
+
+import team.msg.sms.common.annotation.Service
+import team.msg.sms.domain.languagecertificate.model.LanguageCertificate
+import team.msg.sms.domain.languagecertificate.service.CommandLanguageCertificateService
+import team.msg.sms.domain.languagecertificate.spi.LanguageCertificatePort
+import team.msg.sms.domain.student.model.Student
+import team.msg.sms.domain.user.model.User
+
+@Service
+class CommandLanguageCertificateServiceImpl(
+    val languageCertificatePort: LanguageCertificatePort
+) : CommandLanguageCertificateService {
+    override fun saveAll(
+        languageCertificate: List<LanguageCertificate>,
+        student: Student,
+        user: User
+    ): List<LanguageCertificate> =
+        languageCertificatePort.saveAll(languageCertificate, student, user)
+
+}
