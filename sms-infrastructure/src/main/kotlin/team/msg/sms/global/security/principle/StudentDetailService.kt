77c39d528210b56e299aa8f7fa3dadc8ed09b74a
huuuunee <s21024@gsm.hs.kr>
2023-05-16 17:55:30 +0900

    :recycle: queryStudentPort -> StudentPort 사용하도록

--- sms-infrastructure/src/main/kotlin/team/msg/sms/global/security/principle/StudentDetailService.kt
+++ sms-infrastructure/src/main/kotlin/team/msg/sms/global/security/principle/StudentDetailService.kt
@@ -3,18 +3,18 @@ package team.msg.sms.global.security.principle
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
-import team.msg.sms.domain.student.spi.QueryStudentPort
+import team.msg.sms.domain.student.spi.StudentPort
 import team.msg.sms.global.security.exception.InvalidTokenException
 import java.util.*
 
 @Component
 class StudentDetailService(
-    val queryStudentPort: QueryStudentPort
+    private val studentPort: StudentPort
 ) : UserDetailsService {
     override fun loadUserByUsername(username: String?): UserDetails {
         val studentId = UUID.fromString(username)
 
-        if (queryStudentPort.existsStudentById(studentId))
+        if (studentPort.existsStudentById(studentId))
             throw InvalidTokenException
 
         return StudentDetail(studentId)
