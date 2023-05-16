713b397e90fe9b8837ed35bdc1938fc2d0b5f9f4
huuuunee <s21024@gsm.hs.kr>
2023-05-16 17:48:09 +0900

    :sparkles: MutlipartFile 을 File 하 시키느 Extension 생성

---
+++ sms-presentation/src/main/kotlin/team/msg/sms/common/extension/FileExtension.kt
@@ -0,0 +1,15 @@
+package team.msg.sms.common.extension
+
+import org.springframework.web.multipart.MultipartFile
+import java.io.File
+import java.io.FileOutputStream
+import java.util.UUID
+
+fun MultipartFile.toFile() =
+    File("${UUID.randomUUID()}||$originalFilename").let {
+        FileOutputStream(it).run {
+            this.write(bytes)
+            this.close()
+        }
+        it
+    }
