97c124da1b9839ee7a97d75f725b56e48ed24eaf
huuuunee <s21024@gsm.hs.kr>
2023-05-16 17:42:37 +0900

    :sparkles: AwsS3Properties 생성

---
+++ sms-infrastructure/src/main/kotlin/team/msg/sms/thirdparty/aws/stroage/AwsS3Properties.kt
@@ -0,0 +1,10 @@
+package team.msg.sms.thirdparty.aws.stroage
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConfigurationProperties("cloud.aws.s3")
+@ConstructorBinding
+class AwsS3Properties(
+    val bucket: String
+)
