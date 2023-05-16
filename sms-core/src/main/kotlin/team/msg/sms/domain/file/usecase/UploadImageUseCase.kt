4a65fd2bfbbf211988bda73a01146528f07c18cd
huuuunee <s21024@gsm.hs.kr>
2023-05-16 20:40:28 +0900

    :sparkles: 이미지 업로드 UseCase 생성

---
+++ sms-core/src/main/kotlin/team/msg/sms/domain/file/usecase/UploadImageUseCase.kt
@@ -0,0 +1,20 @@
+package team.msg.sms.domain.file.usecase
+
+import team.msg.sms.common.annotation.Service
+import team.msg.sms.common.util.FileUtil.isImageCorrectExtension
+import team.msg.sms.domain.file.exception.FileInvalidExtensionException
+import team.msg.sms.domain.file.spi.UploadFilePort
+import java.io.File
+
+@Service
+class UploadImageUseCase(
+    private val uploadFilePort: UploadFilePort
+) {
+    fun execute(file: File): String {
+        if (file.extension.isImageCorrectExtension()) {
+            file.delete()
+            throw FileInvalidExtensionException
+        }
+        return uploadFilePort.upload(file)
+    }
+}
