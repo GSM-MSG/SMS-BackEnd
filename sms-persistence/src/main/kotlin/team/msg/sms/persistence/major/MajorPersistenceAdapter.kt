1a604e02b9ed1f3b5ab13378c6e62d44dd82423c
huuuunee <s21024@gsm.hs.kr>
2023-05-17 20:41:23 +0900

    :sparkles: Repository에 접근할 수 있는 Persistence 생성

---
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/major/MajorPersistenceAdapter.kt
@@ -0,0 +1,17 @@
+package team.msg.sms.persistence.major
+
+import org.springframework.stereotype.Component
+import team.msg.sms.domain.major.model.Major
+import team.msg.sms.domain.major.spi.MajorPort
+import team.msg.sms.persistence.major.mapper.toDomain
+import team.msg.sms.persistence.major.repository.MajorJpaRepository
+
+@Component
+class MajorPersistenceAdapter(
+    private val majorJpaRepository: MajorJpaRepository
+) : MajorPort {
+    override fun queryAll(): List<Major> =
+        majorJpaRepository.findAll().map {
+            it.toDomain()
+        }
+}
