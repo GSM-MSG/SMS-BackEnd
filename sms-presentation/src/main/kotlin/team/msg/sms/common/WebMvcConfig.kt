646ffe500b881d48538227f0823d0ba8e999d6be
huuuunee <s21024@gsm.hs.kr>
2023-05-21 22:52:49 +0900

    :sparkles: WebMvcConfig 생성

---
+++ sms-presentation/src/main/kotlin/team/msg/sms/common/WebMvcConfig.kt
@@ -0,0 +1,18 @@
+package team.msg.sms.common
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedOrigins(
+                "http://localhost:3000",
+            )
+            .exposedHeaders(HttpHeaders.CONTENT_DISPOSITION)
+    }
+}
