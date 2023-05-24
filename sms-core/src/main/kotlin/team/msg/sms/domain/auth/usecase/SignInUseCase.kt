951b9a7df8e7223ac8df812f893e42eb6f4769d1
huuuunee <s21024@gsm.hs.kr>
2023-05-24 18:11:15 +0900

    :sparkles: try catch 문 추가

--- sms-core/src/main/kotlin/team/msg/sms/domain/auth/usecase/SignInUseCase.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/auth/usecase/SignInUseCase.kt
@@ -1,13 +1,18 @@
 package team.msg.sms.domain.auth.usecase
 
+import gauth.exception.GAuthException
 import team.msg.sms.domain.auth.dto.request.SignInData
 import team.msg.sms.common.annotation.UseCase
 import team.msg.sms.domain.auth.dto.response.SignInResponse
 import team.msg.sms.common.spi.GAuthPort
+import team.msg.sms.domain.auth.exception.ExpiredCodeException
+import team.msg.sms.domain.auth.exception.SecretMismatchException
+import team.msg.sms.domain.auth.exception.ServiceNotFoundException
 import team.msg.sms.domain.auth.model.RefreshToken
 import team.msg.sms.domain.auth.spi.JwtPort
 import team.msg.sms.domain.auth.spi.RefreshTokenPort
 import team.msg.sms.domain.student.service.StudentService
+import team.msg.sms.domain.user.exception.InternalServerErrorException
 import team.msg.sms.domain.user.model.User
 import team.msg.sms.domain.user.service.UserService
 
@@ -20,35 +25,44 @@ class SignInUseCase(
     private val studentService: StudentService
 ) {
     fun execute(request: SignInData): SignInResponse {
-        val gAuthToken = gAuthPort.receiveGAuthToken(request.code)
-        val gAuthUserInfo = gAuthPort.receiveUserInfo(gAuthToken.accessToken)
+        try {
+            val gAuthToken = gAuthPort.receiveGAuthToken(request.code)
+            val gAuthUserInfo = gAuthPort.receiveUserInfo(gAuthToken.accessToken)
 
-        val role = userService.getRoleByGAuthInfo(gAuthUserInfo.email, gAuthUserInfo.role)
+            val role = userService.getRoleByGAuthInfo(gAuthUserInfo.email, gAuthUserInfo.role)
 
-        val isExistsUser = userService.checkUserExistByEmail(gAuthUserInfo.email)
-        val user = userService.createUserWhenNotExistUser(
-            isExistsUser,
-            User(
-                name = gAuthUserInfo.name,
-                email = gAuthUserInfo.email,
-                stuNum = if (role.name == "STUDENT") "${gAuthUserInfo.grade}${gAuthUserInfo.classNum}" + if (gAuthUserInfo.num < 10) {
-                    "0${gAuthUserInfo.num}"
-                } else gAuthUserInfo.num else "",
-                roles = mutableListOf(role)
+            val isExistsUser = userService.checkUserExistByEmail(gAuthUserInfo.email)
+            val user = userService.createUserWhenNotExistUser(
+                isExistsUser,
+                User(
+                    name = gAuthUserInfo.name,
+                    email = gAuthUserInfo.email,
+                    stuNum = if (role.name == "STUDENT") "${gAuthUserInfo.grade}${gAuthUserInfo.classNum}" + if (gAuthUserInfo.num < 10) {
+                        "0${gAuthUserInfo.num}"
+                    } else gAuthUserInfo.num else "",
+                    roles = mutableListOf(role)
+                )
             )
-        )
-        val (accessToken, accessTokenExp, refreshToken, refreshTokenExp) = jwtPort.receiveToken(user.id, role)
+            val (accessToken, accessTokenExp, refreshToken, refreshTokenExp) = jwtPort.receiveToken(user.id, role)
 
-        refreshTokenPort.saveRefreshToken(RefreshToken(refreshToken, user.id))
+            refreshTokenPort.saveRefreshToken(RefreshToken(refreshToken, user.id))
 
-        val isStudent = studentService.checkNewStudent(user, role.name)
+            val isStudent = studentService.checkNewStudent(user, role.name)
 
-        return SignInResponse(
-            accessToken = accessToken,
-            accessTokenExp = accessTokenExp,
-            refreshToken = refreshToken,
-            refreshTokenExp = refreshTokenExp,
-            isExist = isStudent
-        )
+            return SignInResponse(
+                accessToken = accessToken,
+                accessTokenExp = accessTokenExp,
+                refreshToken = refreshToken,
+                refreshTokenExp = refreshTokenExp,
+                isExist = isStudent
+            )
+        } catch (error: GAuthException) {
+            when (error.code) {
+                400 -> throw SecretMismatchException
+                401 -> throw ExpiredCodeException
+                404 -> throw ServiceNotFoundException
+                else -> throw InternalServerErrorException
+            }
+        }
     }
 }
