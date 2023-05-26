a97ba262c80e9d69efa8d88cc16619ca874a5410
huuuunee <s21024@gsm.hs.kr>
2023-05-26 14:13:27 +0900

    :recycle: 로그아웃 쿠키로 요청 왔을때 처리

--- sms-presentation/src/main/kotlin/team/msg/sms/domain/auth/AuthWebAdapter.kt
+++ sms-presentation/src/main/kotlin/team/msg/sms/domain/auth/AuthWebAdapter.kt
@@ -9,6 +9,7 @@ import team.msg.sms.domain.auth.usecase.LogoutUseCase
 import team.msg.sms.domain.auth.usecase.ReIssueTokenUseCase
 import team.msg.sms.domain.auth.usecase.SignInUseCase
 import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 import javax.validation.Valid
 
@@ -42,14 +43,31 @@ class AuthWebAdapter(
             .let { ResponseEntity.ok(it) }
 
     @DeleteMapping
-    fun logout(@Valid @RequestHeader("Refresh-Token") refreshToken: String): ResponseEntity<Void> =
-        logoutUseCase.execute(refreshToken)
-            .let { ResponseEntity.ok().build() }
+    fun logout(
+        @Valid @RequestHeader(name = "Refresh-Token", required = false) refreshToken: String?,
+        httpServletResponse: HttpServletResponse
+    ): ResponseEntity<Void> {
+        if (refreshToken != null) logoutUseCase.execute(refreshToken)
+        else {
+            val accessCookie: Cookie = expiredCookie(httpServletResponse, "accessToken")
+            val refreshCookie: Cookie = expiredCookie(httpServletResponse, "refreshToken")
+
+            httpServletResponse.addCookie(accessCookie)
+            httpServletResponse.addCookie(refreshCookie)
+        }
+        return ResponseEntity.ok().build()
+    }
 
     private fun createCookie(value: String, token: String, maxAge: Int): Cookie {
         val cookie = Cookie(value, token)
         cookie.isHttpOnly = true
         cookie.maxAge = maxAge
+        return cookie
+    }
+
+    private fun expiredCookie(httpServletResponse: HttpServletResponse, name: String): Cookie {
+        val cookie: Cookie = Cookie(name, null)
+        cookie.maxAge = 0
         return cookie
     }
 }
