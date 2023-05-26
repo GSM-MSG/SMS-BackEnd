9ee3f65c876b8836cdac82711e8e08a46152b940
huuuunee <s21024@gsm.hs.kr>
2023-05-26 13:49:49 +0900

    :sparkles: 로그인시 쿠키도 같이 반환하기

--- sms-presentation/src/main/kotlin/team/msg/sms/domain/auth/AuthWebAdapter.kt
+++ sms-presentation/src/main/kotlin/team/msg/sms/domain/auth/AuthWebAdapter.kt
@@ -8,6 +8,8 @@ import team.msg.sms.domain.auth.dto.response.SignInResponse
 import team.msg.sms.domain.auth.usecase.LogoutUseCase
 import team.msg.sms.domain.auth.usecase.ReIssueTokenUseCase
 import team.msg.sms.domain.auth.usecase.SignInUseCase
+import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServletResponse
 import javax.validation.Valid
 
 @RestController
@@ -15,13 +17,24 @@ import javax.validation.Valid
 class AuthWebAdapter(
     private val signInUseCase: SignInUseCase,
     private val reIssueTokenUseCase: ReIssueTokenUseCase,
-    private val logoutUseCase: LogoutUseCase
+    private val logoutUseCase: LogoutUseCase,
 ) {
 
     @PostMapping
-    fun signIn(@Valid @RequestBody request: SignInRequest): ResponseEntity<SignInResponse> =
-        signInUseCase.execute(request.toData())
-            .let { ResponseEntity.ok(it) }
+    fun signIn(
+        @Valid @RequestBody request: SignInRequest,
+        httpServletResponse: HttpServletResponse
+    ): ResponseEntity<SignInResponse> {
+        val token: SignInResponse = signInUseCase.execute(request.toData())
+
+        val accessCookie: Cookie = createCookie("accessToken", token.accessToken, 3600)
+        val refreshCookie: Cookie = createCookie("refreshToken", token.refreshToken, 36000)
+
+        httpServletResponse.addCookie(accessCookie)
+        httpServletResponse.addCookie(refreshCookie)
+
+        return ResponseEntity.ok(token)
+    }
 
     @PatchMapping
     fun reIssueToken(@Valid @RequestHeader("Refresh-Token") header: String): ResponseEntity<ReIssueTokenResponse> =
@@ -32,4 +45,11 @@ class AuthWebAdapter(
     fun logout(@Valid @RequestHeader("Refresh-Token") refreshToken: String): ResponseEntity<Void> =
         logoutUseCase.execute(refreshToken)
             .let { ResponseEntity.ok().build() }
+
+    private fun createCookie(value: String, token: String, maxAge: Int): Cookie {
+        val cookie = Cookie(value, token)
+        cookie.isHttpOnly = true
+        cookie.maxAge = maxAge
+        return cookie
+    }
 }
