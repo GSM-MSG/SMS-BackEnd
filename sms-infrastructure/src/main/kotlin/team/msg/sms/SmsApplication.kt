b3c46a2db8ad577aa1cc190815ae9e5efe08cee6
huuuunee <s21024@gsm.hs.kr>
2023-05-28 17:29:22 +0900

    :sparkles: 서버 실행할떄 타임존을 서울로 등록

--- sms-infrastructure/src/main/kotlin/team/msg/sms/SmsApplication.kt
+++ sms-infrastructure/src/main/kotlin/team/msg/sms/SmsApplication.kt
@@ -2,10 +2,13 @@ package team.msg.sms
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
 class SmsApplication
 
 fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
     runApplication<SmsApplication>(*args)
 }
