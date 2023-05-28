7528240df391d107af715c9b8a35e97c37570971
huuuunee <s21024@gsm.hs.kr>
2023-05-28 17:31:11 +0900

    :fire: 안쓰는 Import삭제

--- sms-infrastructure/src/main/kotlin/team/msg/sms/SmsApplication.kt
+++ sms-infrastructure/src/main/kotlin/team/msg/sms/SmsApplication.kt
@@ -3,7 +3,6 @@ package team.msg.sms
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import java.util.TimeZone
-import javax.annotation.PostConstruct
 
 @SpringBootApplication
 class SmsApplication
