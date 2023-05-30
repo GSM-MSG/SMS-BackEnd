22b6ced1eef45e9dc544fed2c687003ff5505916
전승원 <78971821+louis7308@users.noreply.github.com>
2023-05-30 11:11:09 +0900

    :sparkles: :: Url 접근 불가 filter 로직 추가

--- sms-infrastructure/src/main/kotlin/team/msg/sms/global/filter/FilterConfig.kt
+++ sms-infrastructure/src/main/kotlin/team/msg/sms/global/filter/FilterConfig.kt
@@ -6,16 +6,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.stereotype.Component
+import team.msg.sms.domain.student.spi.QueryStudentPort
+import team.msg.sms.domain.user.spi.QueryUserPort
 import team.msg.sms.global.security.token.JwtParser
 
 @Component
 class FilterConfig(
     private val jwtParser: JwtParser,
-    private val objectMapper: ObjectMapper
+    private val objectMapper: ObjectMapper,
+    private val studentPort: QueryStudentPort,
+    private val userPort: QueryUserPort
 ) : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
     override fun configure(builder: HttpSecurity) {
+        builder.addFilterBefore(ExceptionFilter(objectMapper), UsernamePasswordAuthenticationFilter::class.java)
+        builder.addFilterBefore(CookieJwtFilter(jwtParser, studentPort, userPort), UsernamePasswordAuthenticationFilter::class.java)
         builder.addFilterBefore(JwtFilter(jwtParser), UsernamePasswordAuthenticationFilter::class.java)
-        builder.addFilterBefore(CookieJwtFilter(jwtParser), JwtFilter::class.java)
-        builder.addFilterBefore(ExceptionFilter(objectMapper), CookieJwtFilter::class.java)
     }
 }
