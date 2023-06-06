97f4ae2f3af6356c229b03063b3697ee548d2ea9
전승원 <78971821+louis7308@users.noreply.github.com>
2023-06-06 21:51:15 +0900

    :sparkles: :: page & size 값을 받게 추가

--- sms-presentation/src/main/kotlin/team/msg/sms/domain/student/StudentWebAdapter.kt
+++ sms-presentation/src/main/kotlin/team/msg/sms/domain/student/StudentWebAdapter.kt
@@ -1,19 +1,32 @@
 package team.msg.sms.domain.student
 
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import team.msg.sms.domain.student.dto.SignUpWebRequest
+import team.msg.sms.domain.student.dto.response.StudentInfoListResponse
+import team.msg.sms.domain.student.usecase.FindAllUseCase
 import team.msg.sms.domain.student.usecase.SignUpUseCase
 import javax.validation.Valid
 
 @RestController
 @RequestMapping("/student")
 class StudentWebAdapter(
-    private val signUpUseCase: SignUpUseCase
+    private val signUpUseCase: SignUpUseCase,
+    private val findAllUseCase: FindAllUseCase
 ) {
+    @GetMapping
+    fun findAll(
+        @RequestParam(name = "page") page: Int,
+        @RequestParam(name = "size") size: Int
+    ): ResponseEntity<StudentInfoListResponse> =
+        findAllUseCase.execute(page, size)
+            .let { ResponseEntity.ok(it) }
+
     @PostMapping
     fun signUpStudent(@RequestBody @Valid signUpWebRequest: SignUpWebRequest): ResponseEntity<Void> =
         signUpUseCase.execute(signUpWebRequest.toData())
