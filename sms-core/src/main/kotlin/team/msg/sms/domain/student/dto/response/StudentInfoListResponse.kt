8d85d1755431c3482001597aa03304d99b6e2798
전승원 <78971821+louis7308@users.noreply.github.com>
2023-06-06 21:45:24 +0900

    :sparkles: :: StudentInfoResponse

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/dto/response/StudentInfoListResponse.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/dto/response/StudentInfoListResponse.kt
@@ -1,4 +1,17 @@
 package team.msg.sms.domain.student.dto.response
 
-class StudentInfoListResponse {
+
+data class StudentInfoListResponse(
+    val content: List<StudentContentInfo>,
+    val page: Int,
+    val last: Boolean,
+    val size: Int,
+) {
+    data class StudentContentInfo(
+        val profileImg: String,
+        val name: String,
+        val major: String,
+        val techStack: List<String>
+    )
 }
+
