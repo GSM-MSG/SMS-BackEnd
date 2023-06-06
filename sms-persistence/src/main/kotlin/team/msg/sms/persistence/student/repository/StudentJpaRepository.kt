0cbdfd257af064aeb225d5eac460c18fb310e439
전승원 <78971821+louis7308@users.noreply.github.com>
2023-06-06 21:50:48 +0900

    :sparkles: :: Fetch Join으로 N + 1 해결하기

--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/student/repository/StudentJpaRepository.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/student/repository/StudentJpaRepository.kt
@@ -1,6 +1,8 @@
 package team.msg.sms.persistence.student.repository
 
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import team.msg.sms.persistence.student.entity.StudentJpaEntity
 import team.msg.sms.persistence.user.entity.UserJpaEntity
@@ -9,4 +11,7 @@ import java.util.UUID
 @Repository
 interface StudentJpaRepository : JpaRepository<StudentJpaEntity, UUID> {
     fun existsByUser(user: UserJpaEntity): Boolean
+
+    @Query("select s from StudentJpaEntity  s join fetch s.user")
+    fun findWithPagination(pageable: Pageable): List<StudentJpaEntity>
 }
