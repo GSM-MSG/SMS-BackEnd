172729dbabaf4d733035864755866f68fb867d24
전승원 <78971821+louis7308@users.noreply.github.com>
2023-06-09 08:26:51 +0900

    :sparkles: :: Service어노테이션 추가하여 순함참조 해결

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/service/impl/GetStudentServiceImpl.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/service/impl/GetStudentServiceImpl.kt
@@ -1,9 +1,11 @@
 package team.msg.sms.domain.student.service.impl
 
+import team.msg.sms.common.annotation.Service
 import team.msg.sms.domain.student.model.Student
 import team.msg.sms.domain.student.service.GetStudentService
 import team.msg.sms.domain.student.spi.StudentPort
 
+@Service
 class GetStudentServiceImpl(
     private val studentPort: StudentPort
 ) : GetStudentService {
