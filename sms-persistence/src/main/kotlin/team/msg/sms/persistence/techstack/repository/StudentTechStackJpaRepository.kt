82edc789192347b31d3e86c1cc01eb6871f96184
전승원 <78971821+louis7308@users.noreply.github.com>
2023-06-09 08:50:22 +0900

    :ambulance: :: TechStack Entity 수정

--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/techstack/repository/StudentTechStackJpaRepository.kt
+++
@@ -1,7 +0,0 @@
-package team.msg.sms.persistence.techstack.repository
-
-import org.springframework.data.repository.CrudRepository
-import team.msg.sms.persistence.techstack.entity.StudentTechStackJpaEntity
-
-interface StudentTechStackJpaRepository : CrudRepository<StudentTechStackJpaEntity, Long> {
-}
