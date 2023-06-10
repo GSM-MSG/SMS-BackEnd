2aa3e36845aa3caae239490819102ace6446ee46
전승원 <78971821+louis7308@users.noreply.github.com>
2023-06-10 21:59:29 +0900

    :sparkles: :: 기술스택이 연결된 상태 students 값을 반환

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/service/GetStudentService.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/service/GetStudentService.kt
@@ -1,7 +1,10 @@
 package team.msg.sms.domain.student.service
 
 import team.msg.sms.domain.student.model.Student
+import team.msg.sms.domain.techstack.model.TechStack
 
 interface GetStudentService {
     fun getStudentsWithPage(page: Int, size: Int): Student.StudentWithPageInfo
+
+    fun matchStudentWithTechStacks(students: List<Student.StudentWithUserInfo>, techStacks: List<TechStack>): List<Student.StudentWithUserInfo>
 }
--- sms-core/src/main/kotlin/team/msg/sms/domain/student/usecase/FindAllUseCase.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/usecase/FindAllUseCase.kt
@@ -1,18 +1,28 @@
 package team.msg.sms.domain.student.usecase
 
 import team.msg.sms.common.annotation.UseCase
+import team.msg.sms.common.service.SecurityService
 import team.msg.sms.domain.student.dto.response.StudentInfoListResponse
 import team.msg.sms.domain.student.service.StudentService
+import team.msg.sms.domain.techstack.service.TechStackService
 
 @UseCase
 class FindAllUseCase(
-    private val studentService: StudentService
+    private val studentService: StudentService,
+    private val techStackService: TechStackService,
+    private val securityService: SecurityService
 ) {
     fun execute(page: Int, size: Int): StudentInfoListResponse {
         val studentsWithPageInfo = studentService.getStudentsWithPage(page, size)
+        val techStacks = techStackService.getAllTechStack()
+
+        val students = studentService.matchStudentWithTechStacks(studentsWithPageInfo.students, techStacks)
+
+        val currentRole = securityService.getCurrentUserRole()
+
 
         return StudentInfoListResponse(
-            content = studentsWithPageInfo.students,
+            content = students,
             page = studentsWithPageInfo.page,
             contentSize = studentsWithPageInfo.contentSize,
             totalSize = studentsWithPageInfo.totalSize,
