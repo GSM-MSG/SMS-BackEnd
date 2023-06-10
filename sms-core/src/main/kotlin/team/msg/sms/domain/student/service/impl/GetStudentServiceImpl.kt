9a60e30cbd22d68c9d40c2ce0c543c450bc58b95
전승원 <78971821+louis7308@users.noreply.github.com>
2023-06-10 21:59:29 +0900

    :sparkles: :: 기술스택이 연결된 상태 students 값을 반환

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/service/impl/GetStudentServiceImpl.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/service/impl/GetStudentServiceImpl.kt
@@ -4,6 +4,7 @@ import team.msg.sms.common.annotation.Service
 import team.msg.sms.domain.student.model.Student
 import team.msg.sms.domain.student.service.GetStudentService
 import team.msg.sms.domain.student.spi.StudentPort
+import team.msg.sms.domain.techstack.model.TechStack
 
 @Service
 class GetStudentServiceImpl(
@@ -11,4 +12,14 @@ class GetStudentServiceImpl(
 ) : GetStudentService {
     override fun getStudentsWithPage(page: Int, size: Int): Student.StudentWithPageInfo =
         studentPort.getStudentsWithPage(page, size)
+
+    override fun matchStudentWithTechStacks(students: List<Student.StudentWithUserInfo>, techStacks: List<TechStack>): List<Student.StudentWithUserInfo> {
+        return students.map { student ->
+            val matchingTechStacks = techStacks
+                .filter { techStack -> techStack.studentId == student.id }
+                .map { techStack -> techStack.stack }
+
+            student.copy(techStack = matchingTechStacks)
+        }
+    }
 }
