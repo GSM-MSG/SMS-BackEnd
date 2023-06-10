19a7006162a0ba144a92ed7d55cfa41dc384aeb2
전승원 <78971821+louis7308@users.noreply.github.com>
2023-06-10 21:57:41 +0900

    :sparkles: :: SecurityContext 에서 현재 Role 가져오는 함수

--- sms-core/src/main/kotlin/team/msg/sms/common/service/SecurityService.kt
+++ sms-core/src/main/kotlin/team/msg/sms/common/service/SecurityService.kt
@@ -1,7 +1,10 @@
 package team.msg.sms.common.service
 
+import team.msg.sms.domain.auth.model.Role
 import java.util.*
 
 interface SecurityService {
     fun getCurrentUserId(): UUID
+
+    fun getCurrentUserRole(): String
 }
