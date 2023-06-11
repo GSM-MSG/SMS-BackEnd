fc0ebce1209ba3754a01289a7593ab88ae18b848
전승원 <78971821+louis7308@users.noreply.github.com>
2023-06-12 00:56:49 +0900

    :sparkles: :: /anonymous/{uuid} adapter 추가

--- sms-presentation/src/main/kotlin/team/msg/sms/domain/student/StudentWebAdapter.kt
+++ sms-presentation/src/main/kotlin/team/msg/sms/domain/student/StudentWebAdapter.kt
@@ -3,16 +3,22 @@ package team.msg.sms.domain.student
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import team.msg.sms.domain.student.dto.SignUpWebRequest
+import team.msg.sms.domain.student.dto.response.DetailStudentInfoAnonymousResponse
+import team.msg.sms.domain.student.dto.response.DetailStudentInfoResponse
 import team.msg.sms.domain.student.dto.response.StudentInfoListResponse
 import team.msg.sms.domain.student.usecase.FindAllUseCase
 import team.msg.sms.domain.student.usecase.SignUpUseCase
+import team.msg.sms.domain.student.usecase.StudentInfoAnonymousUseCase
+import team.msg.sms.domain.student.usecase.StudentInfoDetailUseCase
 import javax.validation.Valid
 
 @RestController
 @RequestMapping("/student")
 class StudentWebAdapter(
     private val signUpUseCase: SignUpUseCase,
-    private val findAllUseCase: FindAllUseCase
+    private val findAllUseCase: FindAllUseCase,
+    private val studentInfoAnonymousUseCase: StudentInfoAnonymousUseCase,
+    private val studentInfoDetailUseCase: StudentInfoDetailUseCase
 ) {
     @GetMapping
     fun findAll(
@@ -27,4 +33,10 @@ class StudentWebAdapter(
     fun signUpStudent(@RequestBody @Valid signUpWebRequest: SignUpWebRequest): ResponseEntity<Void> =
         signUpUseCase.execute(signUpWebRequest.toData())
             .run { ResponseEntity.ok().build() }
+
+    @GetMapping("/anonymous/{uuid}")
+    fun findForAnonymousRole(@PathVariable uuid: String): ResponseEntity<DetailStudentInfoAnonymousResponse> =
+        studentInfoAnonymousUseCase.execute(uuid)
+            .let { ResponseEntity.ok(it) }
+        studentInfoDetailUseCase.execute(uuid)
 }
