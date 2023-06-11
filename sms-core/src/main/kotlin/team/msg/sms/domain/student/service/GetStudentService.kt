25e8ca62045a7f1cc719f1f49eaa81285e050962
huuuunee <s21024@gsm.hs.kr>
2023-06-11 23:47:11 +0900

    :twisted_rightwards_arrows: 병합

---
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/service/GetStudentService.kt
@@ -0,0 +1,14 @@
+package team.msg.sms.domain.student.service
+
+import team.msg.sms.domain.student.model.Student
+import team.msg.sms.domain.techstack.model.TechStack
+
+interface GetStudentService {
+    fun getStudentsWithPage(page: Int, size: Int): Student.StudentWithPageInfo
+
+    fun matchStudentWithTechStacks(
+        students: List<Student.StudentWithUserInfo>,
+        techStacks: List<TechStack>,
+        role: String
+    ): List<Student.StudentWithUserInfo>
+}
---
+++ sms-core/src/main/kotlin/team/msg/sms/domain/techstack/service/impl/QueryTechStackServiceImpl.kt
@@ -0,0 +1,17 @@
+package team.msg.sms.domain.techstack.service.impl
+
+import org.springframework.stereotype.Service
+import team.msg.sms.domain.techstack.model.TechStack
+import team.msg.sms.domain.techstack.service.QueryTechStackService
+import team.msg.sms.domain.techstack.spi.TechStackPort
+
+@Service
+class QueryTechStackServiceImpl(
+    private val techStackPort: TechStackPort
+) : QueryTechStackService {
+    override fun getAllTechStack(): List<TechStack> =
+        techStackPort.queryAll()
+
+    override fun getAllTechStackByStack(stack: String): List<TechStack> =
+        techStackPort.queryAllByStack(stack)
+}
