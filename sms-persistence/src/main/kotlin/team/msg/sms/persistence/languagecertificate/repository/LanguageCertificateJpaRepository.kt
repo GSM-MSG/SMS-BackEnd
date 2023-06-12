c8f743cd997294c3367e3306bc0ba3df040c95eb
huuuunee <s21024@gsm.hs.kr>
2023-06-12 22:21:25 +0900

    :sparkles: [LanguageCertificate] deleteByStudent 함수 추가

--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/languagecertificate/repository/LanguageCertificateJpaRepository.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/languagecertificate/repository/LanguageCertificateJpaRepository.kt
@@ -3,9 +3,11 @@ package team.msg.sms.persistence.languagecertificate.repository
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import team.msg.sms.persistence.languagecertificate.entity.LanguageCertificateJpaEntity
+import team.msg.sms.persistence.student.entity.StudentJpaEntity
 import java.util.UUID
 
 @Repository
 interface LanguageCertificateJpaRepository : CrudRepository<LanguageCertificateJpaEntity, Long> {
     fun findByStudentId(uuid: UUID): List<LanguageCertificateJpaEntity>
+    fun deleteAllByStudent(studentJpaEntity: StudentJpaEntity)
 }
