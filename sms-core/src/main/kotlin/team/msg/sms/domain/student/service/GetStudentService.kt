9d2791035c564e81f5493ba2a1688242d40c427f
huuuunee <s21024@gsm.hs.kr>
2023-06-12 22:24:37 +0900

    :sparkles: 유저로 학생을 찾아오는 getStudentByUser

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/service/GetStudentService.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/service/GetStudentService.kt
@@ -2,6 +2,7 @@ package team.msg.sms.domain.student.service
 
 import team.msg.sms.domain.student.model.Student
 import team.msg.sms.domain.techstack.model.TechStack
+import team.msg.sms.domain.user.model.User
 
 interface GetStudentService {
     fun getStudentsWithPage(page: Int, size: Int): Student.StudentWithPageInfo
@@ -13,5 +14,6 @@ interface GetStudentService {
     ): List<Student.StudentWithUserInfo>
 
     fun getStudentByUuid(uuid: String): Student.StudentWithUserInfo
+    fun getStudentByUser(user: User): Student
     fun currentStudent(): Student
 }
