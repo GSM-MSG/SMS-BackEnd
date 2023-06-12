36e61463dde9970cb735fd3b67ba2bf55f4666b4
전승원 <78971821+louis7308@users.noreply.github.com>
2023-06-12 10:21:45 +0900

    :sparkles: :: 선생님 권한으로 디테일 페이지 데이터 추가

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/dto/response/DetailStudentInfoTeacherResponse.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/dto/response/DetailStudentInfoTeacherResponse.kt
@@ -1,6 +1,9 @@
 package team.msg.sms.domain.student.dto.response
 
+import team.msg.sms.domain.languagecertificate.model.LanguageCertificate
 import team.msg.sms.domain.student.model.Department
+import team.msg.sms.domain.student.model.FormOfEmployment
+import team.msg.sms.domain.student.model.MilitaryService
 
 class DetailStudentInfoTeacherResponse(
     val name: String,
@@ -11,6 +14,14 @@ class DetailStudentInfoTeacherResponse(
     val department: Department,
     val major: String,
     val profileImg: String,
+    val contactEmail: String,
+    val gsmAuthenticationScore: Int,
+    val formOfEmployment: FormOfEmployment,
+    val regions: List<String>, // 근무지역
+    val militaryService: MilitaryService,
+    val salary: Int,
+    val languageCertificates: List<LanguageCertificate>,
+    val certificates: List<String>,
     val techStack: List<String>
 ) {
 }
