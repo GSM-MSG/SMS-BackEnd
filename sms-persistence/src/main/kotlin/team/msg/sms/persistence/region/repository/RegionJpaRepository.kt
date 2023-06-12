8c4653cc7969a16e4709cfb1ad423ce17cceecec
huuuunee <s21024@gsm.hs.kr>
2023-06-12 22:20:59 +0900

    :sparkles: deleteByStudent 함수 추가

--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/region/repository/RegionJpaRepository.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/region/repository/RegionJpaRepository.kt
@@ -3,9 +3,11 @@ package team.msg.sms.persistence.region.repository
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import team.msg.sms.persistence.region.entity.RegionJpaEntity
+import team.msg.sms.persistence.student.entity.StudentJpaEntity
 import java.util.UUID
 
 @Repository
 interface RegionJpaRepository : CrudRepository<RegionJpaEntity, Long>{
     fun findByStudentId(uuid: UUID): List<RegionJpaEntity>
+    fun deleteAllByStudent(studentJpaEntity: StudentJpaEntity)
 }
