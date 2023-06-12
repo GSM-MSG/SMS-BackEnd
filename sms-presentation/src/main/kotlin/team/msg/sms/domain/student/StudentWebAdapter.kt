ed9bdc2bec49553054e8e777aec0ffee42c40831
huuuunee <s21024@gsm.hs.kr>
2023-06-12 09:01:44 +0900

    :recycle: ROLE 붙여서 처리

--- sms-presentation/src/main/kotlin/team/msg/sms/domain/student/StudentWebAdapter.kt
+++ sms-presentation/src/main/kotlin/team/msg/sms/domain/student/StudentWebAdapter.kt
@@ -2,6 +2,7 @@ package team.msg.sms.domain.student
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import team.msg.sms.domain.student.dto.FindAllFiltersWebRequest
 import team.msg.sms.domain.student.dto.SignUpWebRequest
 import team.msg.sms.domain.student.dto.response.StudentInfoListResponse
 import team.msg.sms.domain.student.usecase.FindAllUseCase
@@ -18,13 +19,13 @@ class StudentWebAdapter(
     fun findAll(
         @RequestParam(name = "page") page: Int,
         @RequestParam(name = "size") size: Int,
-//        filterRequest: FindAllFiltersWebRequest
+        filterRequest: FindAllFiltersWebRequest
     ): ResponseEntity<StudentInfoListResponse> =
-        findAllUseCase.execute(page, size)
+        findAllUseCase.execute(page, size, filterRequest.toData())
             .let { ResponseEntity.ok(it) }
 
     @PostMapping
     fun signUpStudent(@RequestBody @Valid signUpWebRequest: SignUpWebRequest): ResponseEntity<Void> =
-        signUpUseCase.execute(signUpWebRequest.toData())
+    signUpUseCase.execute(signUpWebRequest.toData())
             .run { ResponseEntity.ok().build() }
 }
