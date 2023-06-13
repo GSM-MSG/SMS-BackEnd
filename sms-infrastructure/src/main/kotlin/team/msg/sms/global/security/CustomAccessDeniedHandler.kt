249c79ce848c455f1dbe329afd5dae7754b9319d
전승원 <78971821+louis7308@users.noreply.github.com>
2023-06-14 08:54:15 +0900

    :sparkles: :: AccessDeniedHandler 에서 403 에러 처리

--- sms-infrastructure/src/main/kotlin/team/msg/sms/global/security/CustomAccessDeniedHandler.kt
+++ sms-infrastructure/src/main/kotlin/team/msg/sms/global/security/CustomAccessDeniedHandler.kt
@@ -1,19 +1,35 @@
 package team.msg.sms.global.security
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.slf4j.LoggerFactory
+import org.springframework.http.MediaType
 import org.springframework.security.access.AccessDeniedException
 import org.springframework.security.web.access.AccessDeniedHandler
 import org.springframework.stereotype.Component
+import team.msg.sms.global.error.ErrorResponse
 import team.msg.sms.global.security.exception.InvalidRoleException
+import team.msg.sms.global.security.exception.error.SecurityErrorCode
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @Component
-class CustomAccessDeniedHandler : AccessDeniedHandler {
+class CustomAccessDeniedHandler(
+    private val objectMapper: ObjectMapper
+) : AccessDeniedHandler {
+
+    private val log = LoggerFactory.getLogger(this.javaClass.simpleName)
+
     override fun handle(
-        request: HttpServletRequest?,
-        response: HttpServletResponse?,
-        accessDeniedException: AccessDeniedException?
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        accessDeniedException: AccessDeniedException
     ) {
-        throw InvalidRoleException
+        log.error("==========Access Denied==========")
+        val errorCode = SecurityErrorCode.FORBIDDEN
+        val responseString = objectMapper.writeValueAsString(ErrorResponse(errorCode.status(), errorCode.message()))
+        response.characterEncoding = "UTF-8"
+        response.status = errorCode.status()
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.writer.write(responseString)
     }
 }
