11b06994aba7b66960f012fd176bdb4bd4d50e0d
huuuunee <s21024@gsm.hs.kr>
2023-06-14 10:58:49 +0900

    :recycle: [region] query -> get in service

--- sms-core/src/main/kotlin/team/msg/sms/domain/region/service/GetRegionService.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/region/service/GetRegionService.kt
@@ -3,6 +3,6 @@ package team.msg.sms.domain.region.service
 import team.msg.sms.domain.region.model.Region
 import java.util.UUID
 
-interface QueryRegionService {
+interface GetRegionService {
     fun getRegionByStudentUuid(uuid: UUID): List<Region>
 }
