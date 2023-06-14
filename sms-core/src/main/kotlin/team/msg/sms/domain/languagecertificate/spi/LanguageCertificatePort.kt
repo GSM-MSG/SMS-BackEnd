591f30ab52164186e7b0055837fd0cd24ce14697
huuuunee <s21024@gsm.hs.kr>
2023-06-14 11:02:19 +0900

    :fire: 임포트 삭제

--- sms-core/src/main/kotlin/team/msg/sms/domain/languagecertificate/spi/LanguageCertificatePort.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/languagecertificate/spi/LanguageCertificatePort.kt
@@ -1,7 +1,5 @@
 package team.msg.sms.domain.languagecertificate.spi
 
-import team.msg.sms.domain.languagecertificate.service.CommandLanguageCertificateService
-
 interface LanguageCertificatePort :
         CommandLanguageCertificatePort,
         QueryLanguageCertificatePort
