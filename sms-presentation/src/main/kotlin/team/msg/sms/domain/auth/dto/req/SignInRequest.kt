249f972d72a18cd0ee4b50d7ce83e37d9fe91c42
huuuunee <s21024@gsm.hs.kr>
2023-06-14 11:23:58 +0900

    :truck: 폴더 구조 dto -> req -> SignInRequest

--- sms-presentation/src/main/kotlin/team/msg/sms/domain/auth/dto/req/SignInRequest.kt
+++ sms-presentation/src/main/kotlin/team/msg/sms/domain/auth/dto/req/SignInRequest.kt
@@ -1,7 +1,7 @@
-package team.msg.sms.domain.auth.dto
+package team.msg.sms.domain.auth.dto.req
 
 import org.jetbrains.annotations.NotNull
-import team.msg.sms.domain.auth.dto.request.SignInData
+import team.msg.sms.domain.auth.dto.SignInData
 
 data class SignInRequest(
     @field:NotNull
