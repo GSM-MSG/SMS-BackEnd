2cefb0e5b95291a6d9ef98d103ef7c2b23fa1484
huuuunee <s21024@gsm.hs.kr>
2023-06-14 11:01:45 +0900

    :recycle: [major] query -> get in service

--- sms-core/src/main/kotlin/team/msg/sms/domain/major/service/impl/QueryMajorServiceImpl.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/major/service/impl/QueryMajorServiceImpl.kt
@@ -2,13 +2,13 @@ package team.msg.sms.domain.major.service.impl
 
 import team.msg.sms.common.annotation.Service
 import team.msg.sms.domain.major.model.Major
-import team.msg.sms.domain.major.service.QueryMajorService
+import team.msg.sms.domain.major.service.GetMajorService
 import team.msg.sms.domain.major.spi.MajorPort
 
 @Service
 class QueryMajorServiceImpl(
     private val majorPort: MajorPort
-) : QueryMajorService {
+) : GetMajorService {
     override fun getAllMajor(): List<Major> =
         majorPort.queryAll()
 }
