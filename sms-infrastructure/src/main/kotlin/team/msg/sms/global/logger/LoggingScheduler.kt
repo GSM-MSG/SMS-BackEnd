299ff2df8f883a07b8bf2069d38922a49bf3a75c
전승원 <78971821+louis7308@users.noreply.github.com>
2023-06-16 08:57:34 +0900

    :sparkles: :: UTF-8 로 objectMetaData 추가

--- sms-infrastructure/src/main/kotlin/team/msg/sms/global/logger/LoggingScheduler.kt
+++ sms-infrastructure/src/main/kotlin/team/msg/sms/global/logger/LoggingScheduler.kt
@@ -8,7 +8,9 @@ import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import team.msg.sms.thirdparty.aws.stroage.AwsS3Properties
+import java.io.BufferedReader
 import java.io.File
+import java.io.InputStreamReader
 
 @Component
 class LoggingScheduler(
@@ -17,10 +19,9 @@ class LoggingScheduler(
 ) {
     private val log by lazy { LoggerFactory.getLogger(this.javaClass.simpleName) }
 
-    @Scheduled(cron = "59 59 23 * * * ?", zone =  "Asia/Seoul")
+    @Scheduled(cron = "0 */2 * * * ?", zone =  "Asia/Seoul")
     fun sendLog() {
         log.info("--------------------로그 스케줄링이 시작됩니다.-----------------------")
-        log.error("--------------------로그 스케줄링이 시작됩니다.-----------------------")
         val logDir = "./logs/"
         val logDirectory = File(logDir)
         logDirectory.listFiles()
@@ -29,6 +30,7 @@ class LoggingScheduler(
                 val objectMetadata = ObjectMetadata()
                 objectMetadata.contentLength = file.length()
                 objectMetadata.contentType = "text/plain"
+                objectMetadata.contentEncoding = "UTF-8"
                 amazonS3.putObject(
                     PutObjectRequest(awsS3Properties.bucketLog, fileName, file.inputStream(), objectMetadata)
                         .withCannedAcl(CannedAccessControlList.PublicRead)
