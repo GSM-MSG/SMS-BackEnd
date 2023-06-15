854e28190b2cd635e138f06dc56936a5d60b5a60
노가성 <82383294+NohGaSeong@users.noreply.github.com>
2023-06-15 11:38:53 +0900

    :recycle: :: server_start shell 수정

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo docker-compose up
+
+EXIST_BLUE=$(docker compose ps blue | grep Up)
+
+
+if [ -z "$EXIST_BLUE" ]; then
+    echo "blue up"
+    docker compose up -d blue
+    BEFORE_COMPOSE_COLOR="green"
+    AFTER_COMPOSE_COLOR="blue"
+else
+    echo "green up"
+    docker compose -d green
+    BEFORE_COMPOSE_COLOR="blue"
+    AFTER_COMPOSE_COLOR="green"
+fi
+
+sleep 10
+
+EXIST_AFTER=$(docker compose ps ${AFTER_COMPOSE_COLOR}| grep Up)
+if [ -n "$EXIST_AFTER" ]; then
+    docker compose ps ${BEFORE_COMPOSE_COLOR} down
+    echo "$BEFORE_COMPOSE_COLOR down"
+fi
