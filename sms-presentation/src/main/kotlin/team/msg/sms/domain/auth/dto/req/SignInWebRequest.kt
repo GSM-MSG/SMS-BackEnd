418770aeb89653520e44ea04e5dbfd5a93be75c7
huuuunee <s21024@gsm.hs.kr>
2023-06-16 09:57:14 +0900

    :recycle: presentation 에서 사용하는 Dto 네이밍 수정

--- sms-presentation/src/main/kotlin/team/msg/sms/domain/auth/dto/req/SignInWebRequest.kt
+++ sms-presentation/src/main/kotlin/team/msg/sms/domain/auth/dto/req/SignInWebRequest.kt
@@ -1,13 +1,12 @@
 package team.msg.sms.domain.auth.dto.req
 
 import org.jetbrains.annotations.NotNull
-import team.msg.sms.domain.auth.dto.SignInData
 
-data class SignInRequest(
+data class SignInWebRequest(
     @field:NotNull
     val code: String
 ) {
-    fun toData() = SignInData(
+    fun toData() = SignInRequestData(
         code = code
     )
 }
