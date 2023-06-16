e38aaaaf74a8ae3e74c54fe4d5c3701ac413410b
전승원 <78971821+louis7308@users.noreply.github.com>
2023-06-16 15:04:43 +0900

    :sparkles: :: pagination 기본 정보 일때 캐싱 처리

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/usecase/FindAllUseCase.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/usecase/FindAllUseCase.kt
@@ -1,5 +1,7 @@
 package team.msg.sms.domain.student.usecase
 
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.transaction.annotation.Transactional
 import team.msg.sms.common.annotation.UseCase
 import team.msg.sms.common.service.SecurityService
 import team.msg.sms.domain.student.dto.request.FiltersData
@@ -7,6 +9,7 @@ import team.msg.sms.domain.student.dto.response.MainStudentsResponse
 import team.msg.sms.domain.student.dto.response.StudentInfoListResponse
 import team.msg.sms.domain.student.service.StudentService
 import team.msg.sms.domain.techstack.service.TechStackService
+import javax.servlet.http.HttpServletRequest
 
 @UseCase
 class FindAllUseCase(
@@ -14,14 +17,17 @@ class FindAllUseCase(
     private val techStackService: TechStackService,
     private val securityService: SecurityService
 ) {
+    @Transactional
+    @Cacheable(value = ["StudentInfoListResponse"], key = "#root.target.generateCacheKey(#page, #size)", cacheManager = "contentCacheManager", unless = "#root.target.hasNonPageAndSizeParameters(#request)")
     fun execute(page: Int, size: Int, filtersData: FiltersData): StudentInfoListResponse {
-        val studentsWithPageInfo = studentService.getStudentsWithPage(page, size)
-        val techStacks = techStackService.getAllTechStack()
+
+        val studentsWithPageInfo = studentService.getStudentsWithPage(page, size) // 20
+        val techStacks = techStackService.getAllTechStack() // 20
         val currentRole = securityService.getCurrentUserRole()
 
         val students = studentService.matchStudentWithTechStacks(studentsWithPageInfo.students, techStacks, currentRole)
 
-        val filterStudents = studentService.filterStudents(students, filtersData, currentRole)
+        val filterStudents = studentService.filterStudents(students, filtersData, currentRole) // 160
 
         val studentsResponses = filterStudents.map {
             MainStudentsResponse(
@@ -35,9 +41,20 @@ class FindAllUseCase(
         return StudentInfoListResponse(
             content = studentsResponses,
             page = studentsWithPageInfo.page,
-            contentSize = students.size,
+            contentSize = filterStudents.size,
             totalSize = studentsWithPageInfo.totalSize,
             last = studentsWithPageInfo.last
         )
+    }
+
+    fun generateCacheKey(page: Int, size: Int): String {
+        return "$page-$size"
+    }
+
+    fun hasNonPageAndSizeParameters(request: HttpServletRequest): Boolean {
+        val queryString = request.queryString ?: return false
+        val params = queryString.split("&").map { it.split("=")[0] }
+        val nonPageAndSizeParams = params.filter { it != "page" && it != "size" }
+        return nonPageAndSizeParams.isNotEmpty()
     }
 }
