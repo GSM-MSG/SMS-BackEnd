1ec481401853fb330d66caaff54749e382fa929d
시훈 <s21024@gsm.hs.kr>
2023-06-16 20:20:52 +0900

    Update CORS / 때기

--- sms-presentation/src/main/kotlin/team/msg/sms/common/WebMvcConfig.kt
+++ sms-presentation/src/main/kotlin/team/msg/sms/common/WebMvcConfig.kt
@@ -12,7 +12,7 @@ class WebMvcConfig : WebMvcConfigurer {
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedOrigins(
                 "http://localhost:3000",
-                "https://sms.msg-team.com/"
+                "https://sms.msg-team.com"
             )
             .allowedHeaders("*")
             .allowCredentials(true)
