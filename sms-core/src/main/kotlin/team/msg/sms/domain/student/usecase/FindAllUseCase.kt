f136e51b5cf6e2775f9a5b03de8213f627e75c59
전승원 <78971821+louis7308@users.noreply.github.com>
2023-06-28 17:27:50 +0900

    :sparkles: :: 메인 페이지 로딩을 캐싱 처리하였습니다

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/usecase/FindAllUseCase.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/usecase/FindAllUseCase.kt
@@ -18,16 +18,16 @@ class FindAllUseCase(
     private val securityService: SecurityService
 ) {
     @Transactional
-    @Cacheable(value = ["StudentInfoListResponse"], key = "#root.target.generateCacheKey(#page, #size)", cacheManager = "contentCacheManager", unless = "#root.target.hasNonPageAndSizeParameters(#request)")
+    @Cacheable(value = ["StudentInfoListResponse"], key = "#root.target.generateCacheKey(#page, #size)", cacheManager = "contentCacheManager")
     fun execute(page: Int, size: Int, filtersData: FiltersData): StudentInfoListResponse {
 
-        val studentsWithPageInfo = studentService.getStudentsWithPage(page, size) // 20
-        val techStacks = techStackService.getAllTechStack() // 20
+        val studentsWithPageInfo = studentService.getStudentsWithPage(page, size)
+        val techStacks = techStackService.getAllTechStack()
         val currentRole = securityService.getCurrentUserRole()
 
         val students = studentService.matchStudentWithTechStacks(studentsWithPageInfo.students, techStacks, currentRole)
 
-        val filterStudents = studentService.filterStudents(students, filtersData, currentRole) // 160
+        val filterStudents = studentService.filterStudents(students, filtersData, currentRole)
 
         val studentsResponses = filterStudents.map {
             MainStudentsResponse(
@@ -49,12 +49,5 @@ class FindAllUseCase(
 
     fun generateCacheKey(page: Int, size: Int): String {
         return "$page-$size"
-    }
-
-    fun hasNonPageAndSizeParameters(request: HttpServletRequest): Boolean {
-        val queryString = request.queryString ?: return false
-        val params = queryString.split("&").map { it.split("=")[0] }
-        val nonPageAndSizeParams = params.filter { it != "page" && it != "size" }
-        return nonPageAndSizeParams.isNotEmpty()
     }
 }
