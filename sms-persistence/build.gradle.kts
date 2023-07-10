9f56a757abfae451911fcf46ed98f66c57443268
전승원 <78971821+louis7308@users.noreply.github.com>
2023-07-10 09:11:58 +0900

    :heavy_plus_sign: kapt 의존성 추가

--- sms-persistence/build.gradle.kts
+++ sms-persistence/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation(Dependencies.REDIS)
     implementation(Dependencies.SPRING_REDIS)
     implementation(Dependencies.MARIADB)
+    kapt(Dependencies.QUERYDSL_PROCESSOR)
 }
 
 allOpen {
