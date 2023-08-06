9405a89f8d556526be39dfafbfe8bb4fc288a52b
전승원 <78971821+louis7308@users.noreply.github.com>
2023-08-06 17:06:09 +0900

    :ambulance: :: Change to querydsl instead of jpa

--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/student/StudentPersistenceAdapter.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/student/StudentPersistenceAdapter.kt
@@ -1,25 +1,16 @@
 package team.msg.sms.persistence.student
 
-import com.querydsl.core.BooleanBuilder
-import com.querydsl.core.Tuple
 import com.querydsl.jpa.impl.JPAQueryFactory
-import org.springframework.data.domain.PageRequest
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
-import team.msg.sms.domain.student.dto.req.FiltersRequestData
 import team.msg.sms.domain.student.model.Student
 import team.msg.sms.domain.student.spi.StudentPort
 import team.msg.sms.domain.user.model.User
-import team.msg.sms.persistence.major.entity.QMajorJpaEntity
-import team.msg.sms.persistence.region.entity.QRegionJpaEntity
 import team.msg.sms.persistence.student.entity.QStudentJpaEntity
 import team.msg.sms.persistence.student.mapper.toDomain
-import team.msg.sms.persistence.student.mapper.toDomainPageWithUserInfo
 import team.msg.sms.persistence.student.mapper.toDomainWithUserInfo
 import team.msg.sms.persistence.student.mapper.toEntity
 import team.msg.sms.persistence.student.repository.StudentJpaRepository
-import team.msg.sms.persistence.techstack.entity.QTechStackJpaEntity
-import team.msg.sms.persistence.user.entity.QUserJpaEntity
 import team.msg.sms.persistence.user.mapper.toEntity
 import java.util.*
 
@@ -45,9 +36,6 @@ class StudentPersistenceAdapter(
     override fun existsStudentByUser(user: User): Boolean =
         studentJpaRepository.existsByUser(user.toEntity())
 
-    override fun queryStudentsWithPage(page: Int, size: Int): Student.StudentWithPageInfo =
-        studentJpaRepository.findAll(PageRequest.of(page - 1, size)).toDomainPageWithUserInfo()
-
     override fun queryStudentByUserId(userId: UUID): Student.StudentWithUserInfo? =
         studentJpaRepository.findByUserId(userId)?.toDomainWithUserInfo()
 
@@ -56,33 +44,12 @@ class StudentPersistenceAdapter(
         return student.toDomain()
     }
 
-    override fun searchStudent(filtersRequestData: FiltersRequestData) {
+    override fun searchStudent(): List<Student.StudentWithUserInfo> {
         val student = QStudentJpaEntity.studentJpaEntity
-        val techStack = QTechStackJpaEntity.techStackJpaEntity
-        val user = QUserJpaEntity.userJpaEntity
 
-        queryFactory
-            .select(
-                student.id,
-                student.user.name,
-                student.major,
-                student.profileImgUrl,
-                techStack.stack,
-            )
-            .from(student)
-            .leftJoin(techStack).on(student.eq(techStack.student))
-            .where(eqGrade(filtersRequestData.grade))
+        return queryFactory
+            .selectFrom(student)
             .fetch()
-    }
-
-    private fun eqGrade(grades: List<Int>?): BooleanBuilder? {
-        if(grades == null) return null
-
-        val booleanBuilder = BooleanBuilder()
-        for(grade in grades) {
-            booleanBuilder.or(QUserJpaEntity.userJpaEntity.stuNum.substring(0, 1).eq(grade.toString()))
-        }
-
-        return booleanBuilder
+            .map { it.toDomainWithUserInfo() }
     }
 }
