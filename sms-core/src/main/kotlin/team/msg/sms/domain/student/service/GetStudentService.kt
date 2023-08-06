936221a95dc4c464ba8caa34d317de7f0091fb70
전승원 <78971821+louis7308@users.noreply.github.com>
2023-08-06 17:09:55 +0900

    :ambulance: :: Replace searchStudent

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/service/GetStudentService.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/service/GetStudentService.kt
@@ -5,7 +5,7 @@ import team.msg.sms.domain.techstack.model.TechStack
 import team.msg.sms.domain.user.model.User
 
 interface GetStudentService {
-    fun getStudentsWithPage(page: Int, size: Int): Student.StudentWithPageInfo
+    fun getStudents(): List<Student.StudentWithUserInfo>
 
     fun matchStudentWithTechStacks(
         students: List<Student.StudentWithUserInfo>,
