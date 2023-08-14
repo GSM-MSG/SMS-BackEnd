da8d533a50afb91c6ebda8ed62e85f67191f364b
huuuunee <s21024@gsm.hs.kr>
2023-08-14 15:14:05 +0900

    :sparkles: ImagePersistenceAdapter 생성

---
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/image/ImagePersistenceAdapter.kt
@@ -0,0 +1,41 @@
+package team.msg.sms.persistence.image
+
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+import team.msg.sms.domain.file.model.Image
+import team.msg.sms.domain.file.spi.ImagePort
+import team.msg.sms.domain.project.exception.ProjectNotFoundException
+import team.msg.sms.domain.project.model.Project
+import team.msg.sms.persistence.image.mapper.toDomain
+import team.msg.sms.persistence.image.mapper.toEntity
+import team.msg.sms.persistence.image.repository.ImageJpaRepository
+import team.msg.sms.persistence.project.repository.ProjectJpaRepository
+
+@Component
+class ImagePersistenceAdapter(
+    private val imageJpaRepository: ImageJpaRepository,
+    private val projectJpaRepository: ProjectJpaRepository
+) : ImagePort {
+    override fun saveAll(images: List<Image>) {
+        val project = projectJpaRepository.findByIdOrNull(images[0].projectId)
+            ?: throw ProjectNotFoundException
+        imageJpaRepository.saveAll(images
+            .map {
+                it.toEntity(project)
+            })
+    }
+
+
+    override fun deleteAllByProjects(projects: List<Project>) {
+        imageJpaRepository.deleteAllByProjects(
+            projects = projects.map {
+                it.id
+            })
+    }
+
+    override fun queryAllByProjectId(projectId: Long): List<Image> =
+        imageJpaRepository.findAllByProjectId(projectId)
+            .map {
+                it.toDomain()
+            }
+}
