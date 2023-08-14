d83d7f599b4dd4efae8ae8d6d599d79395bb0e20
huuuunee <s21024@gsm.hs.kr>
2023-08-14 17:29:26 +0900

    :sparkles: 모든 유저 변경된대로 수정

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/usecase/FindAllUseCase.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/usecase/FindAllUseCase.kt
@@ -6,20 +6,28 @@ import team.msg.sms.domain.student.dto.req.FiltersRequestData
 import team.msg.sms.domain.student.dto.res.MainStudentsResponseData
 import team.msg.sms.domain.student.dto.res.StudentInfoListResponseData
 import team.msg.sms.domain.student.service.StudentService
+import team.msg.sms.domain.student.service.StudentTechStackService
 import team.msg.sms.domain.techstack.service.TechStackService
 
 @UseCase
 class FindAllUseCase(
     private val studentService: StudentService,
     private val techStackService: TechStackService,
-    private val securityService: SecurityService
+    private val securityService: SecurityService,
+    private val studentTechStackService: StudentTechStackService
 ) {
     fun execute(page: Int, size: Int, filtersData: FiltersRequestData): StudentInfoListResponseData {
         val studentsWithPageInfo = studentService.getStudentsWithPage(page, size)
         val techStacks = techStackService.getAllTechStack()
         val currentRole = securityService.getCurrentUserRole()
+        val studentTechStacks = studentTechStackService.getStudentTechStack()
 
-        val students = studentService.matchStudentWithTechStacks(studentsWithPageInfo.students, techStacks, currentRole)
+        val students = studentService.matchStudentWithTechStacks(
+            studentsWithPageInfo.students,
+            techStacks,
+            studentTechStacks,
+            currentRole
+        )
 
         val filterStudents = studentService.filterStudents(students, filtersData, currentRole)
 
