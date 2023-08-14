1cfb94c53a228e5f6bfb1f5f742cd3db1253e5d6
huuuunee <s21024@gsm.hs.kr>
2023-08-14 15:05:04 +0900

    :sparkles: ProjectPersistenceAdapter 생성

---
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/project/ProjectPersistenceAdapter.kt
@@ -0,0 +1,40 @@
+package team.msg.sms.persistence.project
+
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+import team.msg.sms.domain.project.model.Project
+import team.msg.sms.domain.project.spi.ProjectPort
+import team.msg.sms.domain.student.exception.StudentNotFoundException
+import team.msg.sms.domain.student.model.Student
+import team.msg.sms.persistence.project.mapper.toDomain
+import team.msg.sms.persistence.project.mapper.toEntity
+import team.msg.sms.persistence.project.repository.ProjectJpaRepository
+import team.msg.sms.persistence.student.repository.StudentJpaRepository
+import java.util.*
+
+@Component
+class ProjectPersistenceAdapter(
+    val projectRepository: ProjectJpaRepository,
+    val studentRepository: StudentJpaRepository,
+) : ProjectPort {
+    override fun save(project: Project): Project {
+        val student = studentRepository.findByIdOrNull(project.studentId) ?: throw StudentNotFoundException
+        return projectRepository.save(project.toEntity(student)).toDomain()
+    }
+
+    override fun deleteAllByStudent(student: Student) {
+        val student = studentRepository.findByIdOrNull(student.id) ?: throw StudentNotFoundException
+        val project = projectRepository.findAllByStudent(student)
+        projectRepository.deleteAll(project)
+    }
+
+    override fun queryAllProjectByStudentId(studentId: UUID): List<Project> {
+        val student = studentRepository.findByIdOrNull(studentId)
+            ?: throw StudentNotFoundException
+        return projectRepository.findAllByStudent(
+            student = student
+        ).map {
+            it.toDomain()
+        }
+    }
+}
