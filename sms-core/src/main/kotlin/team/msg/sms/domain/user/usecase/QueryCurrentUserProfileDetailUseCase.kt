962e481061a9b3b4ecfd1d15b2ba4ea3ff38a70d
huuuunee <s21024@gsm.hs.kr>
2023-08-14 17:22:25 +0900

    :sparkles: QueryCurrentUserProfileDetailUseCase 수정

--- sms-core/src/main/kotlin/team/msg/sms/domain/user/usecase/QueryCurrentUserProfileDetailUseCase.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/user/usecase/QueryCurrentUserProfileDetailUseCase.kt
@@ -2,11 +2,26 @@ package team.msg.sms.domain.user.usecase
 
 import team.msg.sms.common.annotation.UseCase
 import team.msg.sms.domain.certificate.service.CertificateService
+import team.msg.sms.domain.file.model.Image
+import team.msg.sms.domain.file.service.ImageService
 import team.msg.sms.domain.languagecertificate.model.LanguageCertificate
 import team.msg.sms.domain.languagecertificate.service.LanguageCertificateService
+import team.msg.sms.domain.project.dto.res.ProjectInProgressResponseData
+import team.msg.sms.domain.project.dto.res.ProjectLinkResponseData
+import team.msg.sms.domain.project.dto.res.ProjectResponseData
+import team.msg.sms.domain.project.model.Project
+import team.msg.sms.domain.project.model.ProjectLink
+import team.msg.sms.domain.project.model.ProjectTechStack
+import team.msg.sms.domain.project.service.ProjectLinkService
+import team.msg.sms.domain.project.service.ProjectService
+import team.msg.sms.domain.project.service.ProjectTechStackService
 import team.msg.sms.domain.region.service.RegionService
+import team.msg.sms.domain.student.exception.StudentNotFoundException
 import team.msg.sms.domain.student.model.Student
+import team.msg.sms.domain.student.model.StudentTechStack
 import team.msg.sms.domain.student.service.StudentService
+import team.msg.sms.domain.student.service.StudentTechStackService
+import team.msg.sms.domain.techstack.model.TechStack
 import team.msg.sms.domain.techstack.service.TechStackService
 import team.msg.sms.domain.user.dto.res.UserProfileDetailResponseData
 
@@ -16,24 +31,33 @@ class QueryCurrentUserProfileDetailUseCase(
     private val techStackService: TechStackService,
     private val certificateService: CertificateService,
     private val languageCertificateService: LanguageCertificateService,
+    private val projectService: ProjectService,
+    private val projectTechStackService: ProjectTechStackService,
+    private val projectLinkService: ProjectLinkService,
+    private val imageService: ImageService,
+    private val studentTechStackService: StudentTechStackService,
     private val regionService: RegionService
 ) {
     fun execute(): UserProfileDetailResponseData {
         val student = studentService.currentStudent()
-        val techStacks = techStackService.getTechStackByStudentUuid(student.id).map { it.stack }
+        val techStacks = techStackService.getAllTechStack()
+        val projects = projectService.getAllProjectByStudentId(studentId = student.id)
         val certificates = certificateService.getCertificateByUuid(student.id).map { it.certificateName }
-        val languageCertificates= languageCertificateService.getLanguageCertificateByStudentUuid(student.id)
+        val languageCertificates = languageCertificateService.getLanguageCertificateByStudentUuid(student.id)
             .map { it.toLanguageCertificateScore() }
         val region = regionService.getRegionByStudentUuid(student.id).map { it.region }
-        return toData(student, techStacks, region, languageCertificates, certificates)
+        val studentTechStacks = studentTechStackService.getStudentTechStackByStudentId(studentId = student.id)
+        return toData(student, techStacks, region, studentTechStacks, languageCertificates, certificates, projects)
     }
 
     private fun toData(
         student: Student.StudentWithUserInfo,
-        techStacks: List<String>,
+        techStacks: List<TechStack>,
         regions: List<String>,
+        studentTechStacks: List<StudentTechStack>,
         languageCertificates: List<LanguageCertificate.LanguageCertificateScore>,
-        certificates: List<String>
+        certificates: List<String>,
+        projects: List<Project>
     ): UserProfileDetailResponseData =
         UserProfileDetailResponseData(
             name = student.name,
@@ -54,7 +78,21 @@ class QueryCurrentUserProfileDetailUseCase(
             salary = student.salary,
             languageCertificates = languageCertificates,
             certificates = certificates,
-            techStacks = techStacks
+            studentTechStacks = studentTechStacks.map {
+                toStudentTechStacks(techStacks, it)?.stack ?: ""
+            },
+            projects = projects.map {
+                val image = imageService.getAllByProjectId(projectId = it.id)
+                val link = projectLinkService.getAllByProjectId(projectId = it.id)
+                val projectTechStack = projectTechStackService.getAllByProjectId(projectId = it.id)
+                toProjectResponseData(
+                    project = it,
+                    projectLink = link,
+                    projectImage = image,
+                    projectTechStack = projectTechStack,
+                    techStack = techStacks
+                )
+            }
         )
 
     private fun LanguageCertificate.toLanguageCertificateScore(
@@ -62,5 +100,45 @@ class QueryCurrentUserProfileDetailUseCase(
         LanguageCertificate.LanguageCertificateScore(
             languageCertificateName = this.languageCertificateName,
             score = this.score,
+        )
+
+    private fun toStudentTechStacks(techStacks: List<TechStack>, studentTechStack: StudentTechStack): TechStack? =
+        techStacks.find { it.id == studentTechStack.techStackId }
+
+    private fun toProjectTechStacks(techStacks: List<TechStack>, projectTechStack: ProjectTechStack): TechStack? =
+        techStacks.find { it.id == projectTechStack.techStackId }
+
+    private fun toProjectResponseData(
+        project: Project,
+        projectLink: List<ProjectLink>,
+        projectImage: List<Image>,
+        projectTechStack: List<ProjectTechStack>,
+        techStack: List<TechStack>
+    ): ProjectResponseData =
+        ProjectResponseData(
+            id = project.id,
+            description = project.description,
+            inProgress = toInProgressResponseData(project.startDate, project.endDate),
+            links = projectLink.map { toLinkResponseData(it) },
+            myActivity = project.myActivity,
+            previewImages = projectImage.map { it.imageUrl },
+            projectTechStacks = projectTechStack.map {
+                toProjectTechStacks(techStack, it)?.stack ?: throw StudentNotFoundException
+            },
+            name = project.title,
+        )
+
+
+    private fun toLinkResponseData(projectLink: ProjectLink) =
+        ProjectLinkResponseData(
+            id = projectLink.projectId,
+            name = projectLink.name,
+            url = projectLink.url
+        )
+
+    private fun toInProgressResponseData(start: String, end: String?) =
+        ProjectInProgressResponseData(
+            start = start,
+            end = end
         )
 }
