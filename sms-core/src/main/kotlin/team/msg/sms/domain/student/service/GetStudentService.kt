8049d8eee34c30e73cf71f7391f526cfea17a799
huuuunee <s21024@gsm.hs.kr>
2023-08-14 15:25:41 +0900

    :sparkles: GetStudentService 생성

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/service/GetStudentService.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/service/GetStudentService.kt
@@ -3,6 +3,7 @@ package team.msg.sms.domain.student.service
 import team.msg.sms.domain.student.model.Student
 import team.msg.sms.domain.techstack.model.TechStack
 import team.msg.sms.domain.user.model.User
+import java.util.UUID
 
 interface GetStudentService {
     fun getStudentsWithPage(page: Int, size: Int): Student.StudentWithPageInfo
@@ -13,7 +14,11 @@ interface GetStudentService {
         role: String
     ): List<Student.StudentWithUserInfo>
 
-    fun getStudentByUuid(uuid: String): Student.StudentWithUserInfo
+    fun getStudentUserInfoByUuid(uuid: String): Student.StudentWithUserInfo
+
+    fun getStudentByUuid(uuid: UUID): Student
+
     fun getStudentByUser(user: User): Student
+
     fun currentStudent(): Student.StudentWithUserInfo
 }
