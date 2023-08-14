c03ac49023509383bcb683302b53a7fae30d87b9
huuuunee <s21024@gsm.hs.kr>
2023-08-14 17:31:21 +0900

    :sparkles: StudentInfoAnonymousUseCase 테크 스택 변경된대로 변경

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/usecase/StudentInfoAnonymousUseCase.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/usecase/StudentInfoAnonymousUseCase.kt
@@ -3,24 +3,28 @@ package team.msg.sms.domain.student.usecase
 import team.msg.sms.common.annotation.UseCase
 import team.msg.sms.domain.student.dto.res.DetailStudentInfoAnonymousResponseData
 import team.msg.sms.domain.student.service.StudentService
+import team.msg.sms.domain.student.service.StudentTechStackService
 import team.msg.sms.domain.techstack.service.TechStackService
 
 @UseCase
 class StudentInfoAnonymousUseCase(
     private val studentService: StudentService,
-    private val techStackService: TechStackService
+    private val techStackService: TechStackService,
+    private val studentTechStackService: StudentTechStackService
 ) {
     fun execute(uuid: String): DetailStudentInfoAnonymousResponseData {
-        val student = studentService.getStudentByUuid(uuid)
-        val techStackByStudentUuid = techStackService.getTechStackByStudentUuid(student.id)
-        val techStacks: List<String> = techStackByStudentUuid.map { it.stack }
+        val student = studentService.getStudentUserInfoByUuid(uuid)
+        val techStack = techStackService.getAllTechStack()
+        val studentTechStack = studentTechStackService.getStudentTechStackByStudentId(studentId = student.id)
 
         return DetailStudentInfoAnonymousResponseData(
             name = student.name.replaceRange(1 until student.name.length, "*".repeat(2)),
             introduce = student.introduce,
             major = student.major,
             profileImg = "",
-            techStack = techStacks
+            techStack = studentTechStack.map { studentTechStack ->
+                techStack.find { it.id == studentTechStack.techStackId }?.stack ?: ""
+            }
         )
     }
 }
