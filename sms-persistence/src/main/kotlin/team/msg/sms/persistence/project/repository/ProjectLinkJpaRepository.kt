5f6bf992e745c9ae5ba395e886300443af399db4
huuuunee <s21024@gsm.hs.kr>
2023-08-14 15:10:24 +0900

    :sparkles: ProjectLinkJpaRepository 생성

---
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/project/repository/ProjectLinkJpaRepository.kt
@@ -0,0 +1,14 @@
+package team.msg.sms.persistence.project.repository
+
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
+import team.msg.sms.persistence.project.entity.ProjectLinkJpaEntity
+
+interface ProjectLinkJpaRepository : CrudRepository<ProjectLinkJpaEntity, Long> {
+    fun findAllByProjectId(projectId: Long): List<ProjectLinkJpaEntity>
+    @Modifying
+    @Query("delete from ProjectLinkJpaEntity pt where pt.project.id in :projects")
+    fun deleteAllByProjects(@Param("projects") projects: List<Long>)
+}
