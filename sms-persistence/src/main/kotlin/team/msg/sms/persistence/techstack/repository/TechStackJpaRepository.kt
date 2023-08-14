edec93086246b0f4a9d97481b12af71b76ea4e8f
huuuunee <s21024@gsm.hs.kr>
2023-08-14 14:57:03 +0900

    :sparkles: 기존 테크 스택 가지고 오는거 스택 2 이상인 엔티티만 가져오도록

--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/techstack/repository/TechStackJpaRepository.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/techstack/repository/TechStackJpaRepository.kt
@@ -2,13 +2,10 @@ package team.msg.sms.persistence.techstack.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import team.msg.sms.persistence.student.entity.StudentJpaEntity
 import team.msg.sms.persistence.techstack.entity.TechStackJpaEntity
-import java.util.UUID
 
 @Repository
 interface TechStackJpaRepository : JpaRepository<TechStackJpaEntity, Long> {
-    fun findByStudentId(uuid: UUID): List<TechStackJpaEntity>
     fun findByStackStartingWith(stack: String): List<TechStackJpaEntity>
-    fun deleteAllByStudent(student: StudentJpaEntity)
+    fun findAllByCountGreaterThan(count: Long): List<TechStackJpaEntity>
 }
