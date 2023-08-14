1a3ac684d03c20f8f50669caaa449a2bdd97dbb3
huuuunee <s21024@gsm.hs.kr>
2023-08-14 17:25:42 +0900

    :sparkles: QueryStudentPort 수정

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/spi/QueryStudentPort.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/spi/QueryStudentPort.kt
@@ -6,9 +6,12 @@ import java.util.UUID
 
 interface QueryStudentPort {
     fun queryStudentById(uuid: UUID): Student.StudentWithUserInfo?
+
     fun queryStudentsWithPage(page: Int, size: Int): Student.StudentWithPageInfo
 
-    fun queryStudentByUserId(userId: UUID): Student.StudentWithUserInfo?
+    fun queryStudentByUserId(userId: UUID): Student
+
+    fun queryStudentUserInfoByUserId(userId: UUID): Student.StudentWithUserInfo?
 
     fun queryStudentByUser(user: User): Student
 
