d645503569789fe1c9068f1b3f932997843e3f70
huuuunee <s21024@gsm.hs.kr>
2023-08-17 12:18:36 +0900

    :sparkles: PrizeService 들을 상속받는 PrizeService 생성

---
+++ sms-core/src/main/kotlin/team/msg/sms/domain/prize/service/PrizeService.kt
@@ -0,0 +1,10 @@
+package team.msg.sms.domain.prize.service
+
+import team.msg.sms.common.annotation.Service
+
+@Service
+class PrizeService(
+    commandPrizeService: CommandPrizeService,
+        getPrizeService: GetPrizeService
+) : CommandPrizeService by commandPrizeService,
+    GetPrizeService by getPrizeService
