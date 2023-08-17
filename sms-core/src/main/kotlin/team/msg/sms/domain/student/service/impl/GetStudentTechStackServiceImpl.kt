03c4b17b1cba6b8bac212a0a2d4fe1ba9f151c01
전승원 <78971821+louis7308@users.noreply.github.com>
2023-08-17 20:25:58 +0900

    :sparkles: :: TechStack id 에 맞춰 이름을 매칭 해주는 함수

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/service/impl/GetStudentTechStackServiceImpl.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/service/impl/GetStudentTechStackServiceImpl.kt
@@ -4,6 +4,7 @@ import team.msg.sms.common.annotation.Service
 import team.msg.sms.domain.student.model.StudentTechStack
 import team.msg.sms.domain.student.service.GetStudentTechStackService
 import team.msg.sms.domain.student.spi.StudentTechStackPort
+import team.msg.sms.domain.techstack.model.TechStack
 import java.util.*
 
 @Service
@@ -15,4 +16,10 @@ class GetStudentTechStackServiceImpl(
 
     override fun getStudentTechStack(): List<StudentTechStack> =
         studentTechStackPort.queryStudentTechStack()
+
+    override fun getMatchTechStackNameWithId(studentTechStacks: List<StudentTechStack>, techStacks: List<TechStack>): List<String> =
+        studentTechStacks
+            .mapNotNull { studentTechStack ->
+                techStacks.find { it.id == studentTechStack.techStackId }?.stack
+            }
 }
