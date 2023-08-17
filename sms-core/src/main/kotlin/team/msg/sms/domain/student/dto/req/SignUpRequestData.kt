c6fba5a51d9769ceeb50921030346d20a7604c3e
huuuunee <s21024@gsm.hs.kr>
2023-08-17 12:20:04 +0900

    :sparkles: SignUpRequestData 에 새로 추가된 PrizeRequestData 추가

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/dto/req/SignUpRequestData.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/dto/req/SignUpRequestData.kt
@@ -1,6 +1,7 @@
 package team.msg.sms.domain.student.dto.req
 
 import team.msg.sms.domain.languagecertificate.dto.req.LanguageCertificateRequestData
+import team.msg.sms.domain.prize.dto.req.PrizeRequestData
 import team.msg.sms.domain.project.dto.req.ProjectRequestData
 import team.msg.sms.domain.student.model.FormOfEmployment
 import team.msg.sms.domain.student.model.MilitaryService
@@ -20,5 +21,6 @@ data class SignUpRequestData(
     val dreamBookFileUrl: String,
     val militaryService: MilitaryService,
     val certificate: List<String>,
-    val projects: List<ProjectRequestData>
+    val projects: List<ProjectRequestData>,
+    val prizes: List<PrizeRequestData>
 )
