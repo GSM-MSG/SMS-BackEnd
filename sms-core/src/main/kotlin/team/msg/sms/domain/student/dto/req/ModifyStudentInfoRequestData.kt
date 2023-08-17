bd3eb90cfdda9f671b25b0e56b4153bb39dc11ac
전승원 <78971821+louis7308@users.noreply.github.com>
2023-08-17 10:48:52 +0900

    :sparkles: :: Modify 요청 Dto 생성

---
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/dto/req/ModifyStudentInfoRequestData.kt
@@ -0,0 +1,23 @@
+package team.msg.sms.domain.student.dto.req
+
+import team.msg.sms.domain.languagecertificate.dto.req.LanguageCertificateRequestData
+import team.msg.sms.domain.project.dto.req.ProjectRequestData
+import team.msg.sms.domain.student.model.FormOfEmployment
+import team.msg.sms.domain.student.model.MilitaryService
+
+class ModifyStudentInfoRequestData(
+    val major: String,
+    val studentTechStacks: List<String>,
+    val profileImgUrl: String,
+    val introduce: String,
+    val portfolioUrl: String,
+    val contactEmail: String,
+    val formOfEmployment: FormOfEmployment,
+    val gsmAuthenticationScore: Int,
+    val region: List<String>,
+    val salary: Int,
+    val languageCertificate: List<LanguageCertificateRequestData>,
+    val militaryService: MilitaryService,
+    val certificate: List<String>,
+    val projects: List<ProjectRequestData>
+)
