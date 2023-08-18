66f552dd9dee9fd8e16842214e45663550c86db5
huuuunee <s21024@gsm.hs.kr>
2023-08-18 09:38:50 +0900

    :recycle: [Region] 기존 학생과 유저 정보를 받아서 저장하는 로직에서 모델의 리스트로만 저장하도록 변경

--- sms-core/src/main/kotlin/team/msg/sms/domain/region/service/CommandRegionService.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/region/service/CommandRegionService.kt
@@ -2,9 +2,8 @@ package team.msg.sms.domain.region.service
 
 import team.msg.sms.domain.region.model.Region
 import team.msg.sms.domain.student.model.Student
-import team.msg.sms.domain.user.model.User
 
 interface CommandRegionService {
-    fun saveAll(region: List<Region>, student: Student, user: User): List<Region>
+    fun saveAll(region: List<Region>): List<Region>
     fun deleteAllByStudent(student: Student)
 }
--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/region/RegionPersistenceAdapter.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/region/RegionPersistenceAdapter.kt
@@ -6,13 +6,10 @@ import team.msg.sms.domain.region.model.Region
 import team.msg.sms.domain.region.spi.RegionPort
 import team.msg.sms.domain.student.exception.StudentNotFoundException
 import team.msg.sms.domain.student.model.Student
-import team.msg.sms.domain.user.model.User
 import team.msg.sms.persistence.region.mapper.toDomain
 import team.msg.sms.persistence.region.mapper.toEntity
 import team.msg.sms.persistence.region.repository.RegionJpaRepository
-import team.msg.sms.persistence.student.mapper.toEntity
 import team.msg.sms.persistence.student.repository.StudentJpaRepository
-import team.msg.sms.persistence.user.mapper.toEntity
 import java.util.*
 
 @Component
@@ -20,8 +17,13 @@ class RegionPersistenceAdapter(
     private val regionJpaRepository: RegionJpaRepository,
     private val studentJpaRepository: StudentJpaRepository
 ) : RegionPort {
-    override fun saveAll(region: List<Region>, student: Student, user: User): List<Region> =
-        regionJpaRepository.saveAll(region.map { it.toEntity(student.toEntity(user.toEntity())) }).map { it.toDomain() }
+    override fun saveAll(regions: List<Region>): List<Region> {
+        val student = studentJpaRepository.findByIdOrNull(regions[0].studentId)
+            ?: throw StudentNotFoundException
+        return regionJpaRepository.saveAll(regions
+            .map { it.toEntity(student) })
+            .map { it.toDomain() }
+    }
 
     override fun deleteAllByStudent(student: Student) {
         val student = studentJpaRepository.findByIdOrNull(student.id)
