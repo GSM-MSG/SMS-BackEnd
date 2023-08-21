b4f729fdd79dabc02113f076504ef8be5c57b82c
huuuunee <s21024@gsm.hs.kr>
2023-08-21 11:40:49 +0900

    :sparkles: repository djshxpdltus cnrk

--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/project/repository/ProjectTechStackJpaRepository.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/project/repository/ProjectTechStackJpaRepository.kt
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
 import team.msg.sms.persistence.project.entity.ProjectJpaEntity
 import team.msg.sms.persistence.project.entity.ProjectTechStackJpaEntity
 
+@Repository
 interface ProjectTechStackJpaRepository : CrudRepository<ProjectTechStackJpaEntity, Long> {
     fun findAllByProjectId(projectId: Long): List<ProjectTechStackJpaEntity>
 
