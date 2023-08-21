b25f7607d38fd5bef019e43205e3e27e30e06ace
huuuunee <s21024@gsm.hs.kr>
2023-08-21 11:40:49 +0900

    :sparkles: repository djshxpdltus cnrk

--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/prize/repository/PrizeJpaRepository.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/prize/repository/PrizeJpaRepository.kt
@@ -1,9 +1,11 @@
 package team.msg.sms.persistence.prize.repository
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 import team.msg.sms.persistence.prize.entity.PrizeJpaEntity
 import team.msg.sms.persistence.student.entity.StudentJpaEntity
 
+@Repository
 interface PrizeJpaRepository : CrudRepository<PrizeJpaEntity, Long> {
     fun findAllByStudent(studentJpaEntity: StudentJpaEntity): List<PrizeJpaEntity>
     fun deleteAllByStudent(studentJpaEntity: StudentJpaEntity)
