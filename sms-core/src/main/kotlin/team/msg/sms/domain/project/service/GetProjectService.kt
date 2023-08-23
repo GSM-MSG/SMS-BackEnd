f75a4bc195c5e1e7099f3f5a1c6346bce907973e
전승원 <78971821+louis7308@users.noreply.github.com>
2023-08-23 16:30:45 +0900

    :sparkles: :: 프로젝트들 끼리 매칭 하는 함수

--- sms-core/src/main/kotlin/team/msg/sms/domain/project/service/GetProjectService.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/project/service/GetProjectService.kt
@@ -5,4 +5,5 @@ import java.util.UUID
 
 interface GetProjectService {
     fun getAllProjectByStudentId(studentId: UUID): List<Project>
+    fun getMatchingProject(projects: List<Project>, targetProject: Project): Project
 }
