79facaa3936f6502441d7237da7c61be1d3526d2
전승원 <78971821+louis7308@users.noreply.github.com>
2023-08-23 16:29:25 +0900

    :sparkles: :: 인터페이스에 class 종속성 연결

--- sms-core/src/main/kotlin/team/msg/sms/domain/project/service/ProjectLinkService.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/project/service/ProjectLinkService.kt
@@ -5,6 +5,8 @@ import team.msg.sms.common.annotation.Service
 @Service
 class ProjectLinkService(
     commandProjectLinkService: CommandProjectLinkService,
-    getProjectLinkService: GetProjectLinkService
+    getProjectLinkService: GetProjectLinkService,
+    checkProjectLinkService: CheckProjectLinkService
 ) : CommandProjectLinkService by commandProjectLinkService,
-    GetProjectLinkService by getProjectLinkService
+    GetProjectLinkService by getProjectLinkService,
+    CheckProjectLinkService by checkProjectLinkService
