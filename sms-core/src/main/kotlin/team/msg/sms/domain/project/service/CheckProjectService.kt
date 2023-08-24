de8d2934ea4653be41ddd3eaf2ad91fcfbbf2a3d
전승원 <78971821+louis7308@users.noreply.github.com>
2023-08-24 10:07:44 +0900

    :sparkles: :: 프로젝트 추가 되어 수정 되어져야 하는지 check 하는 로직

--- sms-core/src/main/kotlin/team/msg/sms/domain/project/service/CheckProjectService.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/project/service/CheckProjectService.kt
@@ -3,6 +3,6 @@ package team.msg.sms.domain.project.service
 import team.msg.sms.domain.project.model.Project
 
 interface CheckProjectService {
-    fun checkAddedProject(projects: List<Project>, modifyProject: Project): Project?
+    fun checkAddedProject(projects: List<Project>, modifyProject: Project): Project
     fun checkRemovedProject(projects: List<Project>, modifyProjects: List<Project>): List<Project>
 }
