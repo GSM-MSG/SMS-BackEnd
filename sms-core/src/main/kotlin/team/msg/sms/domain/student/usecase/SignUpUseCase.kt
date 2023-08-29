f961194e4e3cb628b6ada0613c25c94b09d25ed0
전승원 <78971821+louis7308@users.noreply.github.com>
2023-08-29 17:34:29 +0900

    :sparkles: :: project Image 최대 갯수 넘어가면 Exception 처리

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/usecase/SignUpUseCase.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/usecase/SignUpUseCase.kt
@@ -2,6 +2,7 @@ package team.msg.sms.domain.student.usecase
 
 import org.springframework.transaction.annotation.Transactional
 import team.msg.sms.common.annotation.UseCase
+import team.msg.sms.common.util.ProjectUtil
 import team.msg.sms.domain.certificate.model.Certificate
 import team.msg.sms.domain.certificate.service.CertificateService
 import team.msg.sms.domain.file.model.Image
@@ -70,6 +71,7 @@ class SignUpUseCase(
         )
 
         signUpData.projects.forEach {
+            ProjectUtil.validatePreviewImageLimit(it.previewImages)
             val project = projectService.save(project = toProjectModel(it, studentId = student.id))
             projectTechStackValid(techStacks, it.techStacks, project.id)
             saveAllIfNotEmpty(
