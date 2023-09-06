d36c27dfeb3d5cb3d15ed4c8ccb04566ced6ee8b
HumanIearning <kimdh3896@naver.com>
2023-09-06 16:21:10 +0900

    :recycle: :: nginx container가 띄어지기 전 docker exec가 실행돼 오류가 나는 현상을 해결하기 위해 docker up과 docker exec사이에 sleep을 넣어줌

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -14,17 +14,17 @@ if [ -z "$EXIST_BLUE" ]; then
     docker compose up -d blue
     BEFORE_COMPOSE_COLOR="green"
     AFTER_COMPOSE_COLOR="blue"
-    sleep 5
     docker compose rm -svf nginx
+    sleep 1
     docker compose up -d nginx
 else
     echo "green up"
     docker compose up -d green
     BEFORE_COMPOSE_COLOR="blue"
     AFTER_COMPOSE_COLOR="green"
-    sleep 5
     docker compose rm -svf nginx
     docker compose up -d nginx
+    sleep 5
     docker exec nginx sed -i "s|server blue:8080|server green:8090|" /etc/nginx/conf.d/default.conf
 fi
 
