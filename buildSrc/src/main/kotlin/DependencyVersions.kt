4d354cff5c56bbad8f5c8f388d5454a01fc33eb8
huuuunee <s21024@gsm.hs.kr>
2023-09-22 14:37:45 +0900

    :recycle: 테스트 의존성 mockito로 변경

--- buildSrc/src/main/kotlin/DependencyVersions.kt
+++ buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -3,7 +3,7 @@ object DependencyVersions {
     const val JWT_VERSION = "0.9.1"
     const val REDIS_VERSION = "2.7.2"
     const val MYSQL = "8.0.28"
-    const val MOCKK_VERSION = "1.13.2"
+    const val MOCKITO_VERSION = "4.0.0"
     const val AWS_VERSION = "2.2.6.RELEASE"
     const val SERVLET_VERSION = "4.0.1"
     const val GAUTH_VERSION = "2.0.0"
