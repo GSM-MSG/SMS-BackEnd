b88c3beb22de530dd6d5cfa4a97bd1c3a0629bc6
ani2689 <ojno7777@gmail.com>
2023-11-09 16:21:13 +0900

    :fire: 회원가입 시 들어오는 프로필 외의 데이터 삭제

--- sms-core/src/main/kotlin/team/msg/sms/domain/student/dto/req/SignUpRequestData.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/dto/req/SignUpRequestData.kt
@@ -1,11 +1,5 @@
 package team.msg.sms.domain.student.dto.req
 
-import team.msg.sms.domain.languagecertificate.dto.req.LanguageCertificateRequestData
-import team.msg.sms.domain.prize.dto.req.PrizeRequestData
-import team.msg.sms.domain.project.dto.req.ProjectRequestData
-import team.msg.sms.domain.student.model.FormOfEmployment
-import team.msg.sms.domain.student.model.MilitaryService
-
 data class SignUpRequestData(
     val major: String,
     val techStacks: List<String>,
@@ -13,13 +7,4 @@ data class SignUpRequestData(
     val introduce: String,
     val portfolioUrl: String,
     val contactEmail: String,
-    val formOfEmployment: FormOfEmployment,
-    val gsmAuthenticationScore: Int,
-    val regions: List<String>,
-    val salary: Int,
-    val languageCertificates: List<LanguageCertificateRequestData>,
-    val militaryService: MilitaryService,
-    val certificates: List<String>,
-    val projects: List<ProjectRequestData>,
-    val prizes: List<PrizeRequestData>
 )
--- sms-presentation/src/main/kotlin/team/msg/sms/domain/student/dto/req/SignUpWebRequest.kt
+++ sms-presentation/src/main/kotlin/team/msg/sms/domain/student/dto/req/SignUpWebRequest.kt
@@ -1,10 +1,5 @@
 package team.msg.sms.domain.student.dto.req
 
-import team.msg.sms.domain.languagecertificate.dto.req.LanguageCertificateWebRequest
-import team.msg.sms.domain.prize.dto.req.PrizeRequestData
-import team.msg.sms.domain.project.dto.req.ProjectRequestData
-import team.msg.sms.domain.student.model.FormOfEmployment
-import team.msg.sms.domain.student.model.MilitaryService
 import javax.validation.constraints.*
 
 data class SignUpWebRequest(
@@ -27,25 +22,6 @@ data class SignUpWebRequest(
     @field:Email
     val contactEmail: String,
 
-    val regions: List<String>,
-
-    val formOfEmployment: FormOfEmployment,
-
-    @field:NotNull
-    val gsmAuthenticationScore: Int,
-
-    @field:NotNull
-    val salary: Int,
-
-    val languageCertificates: List<LanguageCertificateWebRequest>,
-
-    val militaryService: MilitaryService,
-
-    val certificates: List<String>,
-
-    val projects: List<ProjectRequestData>,
-
-    val prizes: List<PrizeRequestData>
 ) {
     fun toData(): SignUpRequestData =
         SignUpRequestData(
@@ -55,14 +31,5 @@ data class SignUpWebRequest(
             introduce = introduce,
             portfolioUrl = portfolioUrl,
             contactEmail = contactEmail,
-            formOfEmployment = formOfEmployment,
-            gsmAuthenticationScore = gsmAuthenticationScore,
-            regions = regions,
-            salary = salary,
-            languageCertificates = languageCertificates.map { it.toData() },
-            militaryService = militaryService,
-            certificates = certificates,
-            projects = projects,
-            prizes = prizes
         )
 }
