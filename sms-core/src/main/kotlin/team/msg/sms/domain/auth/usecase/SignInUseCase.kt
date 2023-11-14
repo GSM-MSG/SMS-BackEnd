1b615f9bb6cbaf91410e96d5023e5b87b52bed23
ani2689 <ojno7777@gmail.com>
2023-11-14 16:12:33 +0900

    :recycle: 함수로 추출

--- sms-core/src/main/kotlin/team/msg/sms/domain/auth/usecase/SignInUseCase.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/auth/usecase/SignInUseCase.kt
@@ -75,4 +75,6 @@ class SignInUseCase(
 }
 
 private fun getStuNumValid(role: Role, gAuthUserInfo: GAuthUserInfo) =
-    if (role.name != "ROLE_TEACHER") "${gAuthUserInfo.grade}${gAuthUserInfo.classNum}${String.format("%02d", gAuthUserInfo.num)}" else ""
+    if (role.name != "ROLE_TEACHER") "${gAuthUserInfo.grade}${gAuthUserInfo.classNum}${gAuthUserInfo.getNumber()}" else ""
+
+private fun GAuthUserInfo.getNumber() = String.format("%02d", this.num)
