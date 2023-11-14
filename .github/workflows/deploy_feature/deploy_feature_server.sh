815743ba66feff67759672cad533cb610c4eaba3
HumanIearning <kimdh3896@naver.com>
2023-11-14 10:53:02 +0900

    to use variables that are declared in shell script change execute command 'sh' to 'source'

--- .github/workflows/deploy_feature/deploy_feature_server.sh
+++ .github/workflows/deploy_feature/deploy_feature_server.sh
@@ -4,7 +4,6 @@ profile_arn=$FEATURE_SERVER_PROFILE_ARN
 
 # ami = default ubuntu
 instance_id=$(aws ec2 run-instances --image-id ami-086cae3329a3f7d75 --instance-type t2.micro --key-name sms-key --subnet-id subnet-0288374d5ee6ba4c8 --iam-instance-profile Arn="${profile_arn}" --security-group-ids sg-058ec5c5fee753faa --output text --query "Instances[0].InstanceId")
-export instance_id
 
 while [ "$(aws ec2 describe-instances --instance-ids "${instance_id}" --output text --query 'Reservations[*].Instances[*].[State.Name][0][0]')" != "running" ]
 do
@@ -70,6 +69,9 @@ aws ssm list-command-invocations \
     --command-id "${pull_command_id}" \
     --details \
 
+public_ip=$(aws ec2 describe-instances --instance-ids "${instance_id}" --output text --query 'Reservations[*].Instances[*].PublicIpAddress')
+export instance_id
+export public_ip
 #aws ec2 terminate-instances --instance-ids "${instance_id}"
 
 
