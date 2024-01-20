59e64ee6c81cfe43ec350a8eea9ba1ef14114ddb
ani2689 <ojno7777@gmail.com>
2024-01-20 16:47:03 +0900

    :sparkles: exist teacher by user

--- sms-core/src/main/kotlin/team/msg/sms/domain/teacher/spi/QueryTeacherPort.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/teacher/spi/QueryTeacherPort.kt
@@ -1,7 +1,9 @@
 package team.msg.sms.domain.teacher.spi
 
+import team.msg.sms.domain.user.model.User
 import java.util.*
 
 interface QueryTeacherPort {
     fun existsTeacherById(uuid: UUID): Boolean
+    fun existsTeacherByUser(user: User): Boolean
 }
--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/teacher/TeacherPersistenceAdapter.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/teacher/TeacherPersistenceAdapter.kt
@@ -2,7 +2,9 @@ package team.msg.sms.persistence.teacher
 
 import org.springframework.stereotype.Component
 import team.msg.sms.domain.teacher.spi.TeacherPort
+import team.msg.sms.domain.user.model.User
 import team.msg.sms.persistence.teacher.repository.TeacherJpaRepository
+import team.msg.sms.persistence.user.mapper.toEntity
 import java.util.*
 
 @Component
@@ -11,4 +13,8 @@ class TeacherPersistenceAdapter(
 ) : TeacherPort {
     override fun existsTeacherById(uuid: UUID) =
         teacherJpaRepository.existsById(uuid)
+
+    override fun existsTeacherByUser(user: User) =
+        teacherJpaRepository.existsByUser(user.toEntity())
+
 }
