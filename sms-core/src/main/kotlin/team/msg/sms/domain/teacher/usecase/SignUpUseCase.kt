e22072e742f012063242c66f8aee425ab0940cd7
ani2689 <ojno7777@gmail.com>
2024-01-21 22:14:08 +0900

    :sparkles: teacher signup usecase

---
+++ sms-core/src/main/kotlin/team/msg/sms/domain/teacher/usecase/SignUpUseCase.kt
@@ -0,0 +1,21 @@
+package team.msg.sms.domain.teacher.usecase
+
+import org.springframework.transaction.annotation.Transactional
+import team.msg.sms.common.annotation.UseCase
+import team.msg.sms.domain.teacher.service.TeacherService
+import team.msg.sms.domain.user.service.UserService
+
+@UseCase
+class SignUpUseCase(
+    private val userService: UserService,
+    private val teacherService: TeacherService
+) {
+    @Transactional(rollbackFor = [Exception::class])
+    fun execute(){
+        val user = userService.getCurrentUser()
+
+        teacherService.checkTeacherExistsByUser(user)
+
+        teacherService.saveTeacher(user)
+    }
+}
