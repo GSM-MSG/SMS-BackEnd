8c49f03d3629b18769e53cfc27d1fb4e224f9aef
ani2689 <ojno7777@gmail.com>
2024-02-10 13:23:32 +0900

    :bug: teacher id long -> uuid

--- sms-core/src/main/kotlin/team/msg/sms/domain/teacher/model/HomeroomTeacher.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/teacher/model/HomeroomTeacher.kt
@@ -1,11 +1,12 @@
 package team.msg.sms.domain.teacher.model
 
 import team.msg.sms.common.annotation.Aggregate
+import java.util.*
 
 @Aggregate
 data class HomeroomTeacher (
     val id: Long,
     val grade: Int,
     val classNum: Int,
-    val teacherId: Long,
+    val teacherId: UUID,
 )
