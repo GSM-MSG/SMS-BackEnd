088ca995c1c7595e616beefe5573f310f44e0d7b
ani2689 <ojno7777@gmail.com>
2024-02-16 20:30:53 +0900

    :white_check_mark: 함수명 변경

--- sms-core/src/main/kotlin/team/msg/sms/domain/teacher/service/CheckHomeroomTeacherService.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/teacher/service/CheckHomeroomTeacherService.kt
@@ -3,5 +3,5 @@ package team.msg.sms.domain.teacher.service
 import team.msg.sms.domain.teacher.model.Teacher
 
 interface CheckHomeroomTeacherService {
-    fun checkTeacherExistsByTeacher(teacher: Teacher)
+    fun checkHomeroomTeacherExistsByTeacher(teacher: Teacher)
 }
