4482ed2e23a1e470ed3881237d2858e13eb2a80b
ani2689 <ojno7777@gmail.com>
2024-02-16 23:53:18 +0900

    :sparkles: save homeroom teacher

--- sms-core/src/main/kotlin/team/msg/sms/domain/teacher/service/CommandHomeroomTeacherService.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/teacher/service/CommandHomeroomTeacherService.kt
@@ -2,7 +2,8 @@ package team.msg.sms.domain.teacher.service
 
 import team.msg.sms.domain.teacher.model.HomeroomTeacher
 import team.msg.sms.domain.teacher.model.Teacher
+import team.msg.sms.domain.user.model.User
 
 interface CommandHomeroomTeacherService {
-    fun saveHomeroomTeacher(homeroomTeacher: HomeroomTeacher, teacher: Teacher)
+    fun saveHomeroomTeacher(homeroomTeacher: HomeroomTeacher, teacher: Teacher, user: User)
 }
