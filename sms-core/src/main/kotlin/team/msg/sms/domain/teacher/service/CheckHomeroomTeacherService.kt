ab29cb8696e21e6d2451a4ed352d3bc9bce41bb0
ani2689 <ojno7777@gmail.com>
2024-02-16 21:04:46 +0900

    :recycle: 매개변수 변경

--- sms-core/src/main/kotlin/team/msg/sms/domain/teacher/service/CheckHomeroomTeacherService.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/teacher/service/CheckHomeroomTeacherService.kt
@@ -1,7 +1,5 @@
 package team.msg.sms.domain.teacher.service
 
-import team.msg.sms.domain.teacher.model.Teacher
-
 interface CheckHomeroomTeacherService {
-    fun checkHomeroomTeacherExistsByTeacher(teacher: Teacher)
+    fun checkHomeroomTeacherExistsByTeacher(grade: Int, classNum: Int)
 }
