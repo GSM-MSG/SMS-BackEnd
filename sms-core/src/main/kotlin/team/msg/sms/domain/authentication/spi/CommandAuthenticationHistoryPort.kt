0b63e9fd62e30c576e3fa43a6ee3c54d643a8485
ani2689 <ojno7777@gmail.com>
2024-03-04 18:58:59 +0900

    :bug: 매개변수 추가

--- sms-core/src/main/kotlin/team/msg/sms/domain/authentication/spi/CommandAuthenticationHistoryPort.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/authentication/spi/CommandAuthenticationHistoryPort.kt
@@ -1,7 +1,15 @@
 package team.msg.sms.domain.authentication.spi
 
+import team.msg.sms.domain.authentication.model.Authentication
 import team.msg.sms.domain.authentication.model.AuthenticationHistory
+import team.msg.sms.domain.student.model.Student
+import team.msg.sms.domain.user.model.User
 
 interface CommandAuthenticationHistoryPort {
-    fun save(authenticationHistory: AuthenticationHistory): AuthenticationHistory
+    fun save(
+        authenticationHistory: AuthenticationHistory,
+        authentication: Authentication,
+        student: Student,
+        user: User
+    ): AuthenticationHistory
 }
--- sms-core/src/main/kotlin/team/msg/sms/domain/authentication/spi/CommandAuthenticationPort.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/authentication/spi/CommandAuthenticationPort.kt
@@ -1,7 +1,13 @@
 package team.msg.sms.domain.authentication.spi
 
 import team.msg.sms.domain.authentication.model.Authentication
+import team.msg.sms.domain.student.model.Student
+import team.msg.sms.domain.user.model.User
 
 interface CommandAuthenticationPort {
-    fun save(authentication: Authentication): Authentication
+    fun save(
+        authentication: Authentication,
+        student: Student,
+        user: User
+    ): Authentication
 }
