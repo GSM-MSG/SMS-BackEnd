2af49e353cdd910abfe493c9e6c4254d9884053e
ani2689 <ojno7777@gmail.com>
2024-03-04 20:07:35 +0900

    :bug: 타입 변경

--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/authentication/repository/AuthenticationHistoryJpaRepository.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/authentication/repository/AuthenticationHistoryJpaRepository.kt
@@ -2,6 +2,7 @@ package team.msg.sms.persistence.authentication.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import team.msg.sms.persistence.authentication.entity.AuthenticationHistoryJpaEntity
 
 @Repository
-interface AuthenticationHistoryJpaRepository : JpaRepository<AuthenticationHistoryJpaRepository, Long>
+interface AuthenticationHistoryJpaRepository : JpaRepository<AuthenticationHistoryJpaEntity, Long>
