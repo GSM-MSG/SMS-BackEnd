9a983c735722d0408945cd131b54059d670d43d6
ani2689 <ojno7777@gmail.com>
2024-03-04 18:20:41 +0900

    :bug: @Repository 어노테이션

--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/authentication/repository/AuthenticationHistoryJpaRepository.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/authentication/repository/AuthenticationHistoryJpaRepository.kt
@@ -1,5 +1,7 @@
 package team.msg.sms.persistence.authentication.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface AuthenticationHistoryJpaRepository : JpaRepository<AuthenticationHistoryJpaRepository, Long>
--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/authentication/repository/AuthenticationJpaRepository.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/authentication/repository/AuthenticationJpaRepository.kt
@@ -1,7 +1,9 @@
 package team.msg.sms.persistence.authentication.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import team.msg.sms.persistence.authentication.entity.AuthenticationJpaEntity
 import java.util.*
 
+@Repository
 interface AuthenticationJpaRepository : JpaRepository<AuthenticationJpaEntity, UUID>
