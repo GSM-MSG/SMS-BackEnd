b5799153a31bbae83b49deb792f14149d2987686
enbraining <me@xolving.com>
2024-03-05 20:30:00 +0900

    ✨  delete authentication usecase

---
+++ sms-core/src/main/kotlin/team/msg/sms/domain/authentication/usecase/DeleteAuthenticationUseCase.kt
@@ -0,0 +1,22 @@
+package team.msg.sms.domain.authentication.usecase
+
+import org.springframework.context.ApplicationEventPublisher
+import org.springframework.transaction.annotation.Transactional
+import team.msg.sms.common.annotation.UseCase
+import team.msg.sms.domain.authentication.dto.req.CreateAuthenticationRequestData
+import team.msg.sms.domain.authentication.dto.res.CreateAuthenticationResponseData
+import team.msg.sms.domain.authentication.event.AuthenticationHistoryEvent
+import team.msg.sms.domain.authentication.model.Authentication
+import team.msg.sms.domain.authentication.service.AuthenticationService
+import team.msg.sms.domain.student.service.StudentService
+import team.msg.sms.domain.user.service.UserService
+import java.util.*
+
+@UseCase
+class DeleteAuthenticationUseCase(
+    private val studentService: StudentService
+) {
+    fun execute(uuid: UUID) {
+        studentService.deleteByUuid(uuid)
+    }
+}
