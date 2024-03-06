d9bb5fac3d08136a0fef1822a3d801d9c0528805
ani2689 <ojno7777@gmail.com>
2024-03-06 20:31:01 +0900

    :sparkles: response data class

---
+++ sms-presentation/src/main/kotlin/team/msg/sms/domain/authentication/dto/res/QueryRequestedAuthenticationWebResponse.kt
@@ -0,0 +1,13 @@
+package team.msg.sms.domain.authentication.dto.res
+
+import team.msg.sms.domain.student.model.Department
+import java.time.LocalDate
+import java.util.*
+
+data class QueryRequestedAuthenticationWebResponse(
+    val id: UUID,
+    val requestTime: LocalDate,
+    val stuNum: String,
+    val department: Department,
+    val title: String
+)
