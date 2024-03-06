5b1ca89a3ab306cbfae9c61cac4850ce1a89f77e
ani2689 <ojno7777@gmail.com>
2024-03-06 20:11:17 +0900

    :sparkles: filter request data

---
+++ sms-presentation/src/main/kotlin/team/msg/sms/domain/authentication/dto/req/FindRequestAuthenticationFilterWebRequest.kt
@@ -0,0 +1,20 @@
+package team.msg.sms.domain.authentication.dto.req
+
+import org.springframework.web.bind.annotation.RequestParam
+import team.msg.sms.domain.student.model.Department
+
+data class FindRequestAuthenticationFilterWebRequest(
+    @RequestParam("grade", required = false) val grade: List<Int>?,
+    @RequestParam("classNum", required = false) val classNum: List<Int>?,
+    @RequestParam("department", required = false) val department: List<Department>?,
+    @RequestParam("stuNumSort", required = false) val stuNumSort: String?,
+    @RequestParam("requestTimeSort", required = false) val requestTimeSort: String?
+) {
+    fun toData() = FindRequestAuthenticationFilterRequestData(
+        grade = grade,
+        classNum = classNum,
+        department = department,
+        stuNumSort = stuNumSort,
+        requestTimeSort = requestTimeSort
+    )
+}
