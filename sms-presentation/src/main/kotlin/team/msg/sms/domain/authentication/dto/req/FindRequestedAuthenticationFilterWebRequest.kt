50399fd5c40774a6e5871aabd8d0d2c8c0555ce3
ani2689 <ojno7777@gmail.com>
2024-03-06 20:19:13 +0900

    :pencil2: data class 명 변경

--- sms-presentation/src/main/kotlin/team/msg/sms/domain/authentication/dto/req/FindRequestedAuthenticationFilterWebRequest.kt
+++ sms-presentation/src/main/kotlin/team/msg/sms/domain/authentication/dto/req/FindRequestedAuthenticationFilterWebRequest.kt
@@ -3,14 +3,14 @@ package team.msg.sms.domain.authentication.dto.req
 import org.springframework.web.bind.annotation.RequestParam
 import team.msg.sms.domain.student.model.Department
 
-data class FindRequestAuthenticationFilterWebRequest(
+data class FindRequestedAuthenticationFilterWebRequest(
     @RequestParam("grade", required = false) val grade: List<Int>?,
     @RequestParam("classNum", required = false) val classNum: List<Int>?,
     @RequestParam("department", required = false) val department: List<Department>?,
     @RequestParam("stuNumSort", required = false) val stuNumSort: String?,
     @RequestParam("requestTimeSort", required = false) val requestTimeSort: String?
 ) {
-    fun toData() = FindRequestAuthenticationFilterRequestData(
+    fun toData() = FindRequestedAuthenticationFilterRequestData(
         grade = grade,
         classNum = classNum,
         department = department,
