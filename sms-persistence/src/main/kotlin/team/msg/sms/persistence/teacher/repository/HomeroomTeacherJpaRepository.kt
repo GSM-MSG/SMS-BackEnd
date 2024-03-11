6280be042f43461968d96fe0f33da6119b7a8b78
enbraining <me@xolving.com>
2024-03-11 16:04:31 +0900

    ✨ get authentication histories Persistence

--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/teacher/repository/HomeroomTeacherJpaRepository.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/teacher/repository/HomeroomTeacherJpaRepository.kt
@@ -2,9 +2,13 @@ package team.msg.sms.persistence.teacher.repository
 
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import team.msg.sms.domain.teacher.model.HomeroomTeacher
+import team.msg.sms.domain.teacher.model.Teacher
 import team.msg.sms.persistence.teacher.entity.HomeroomTeacherJpaEntity
+import team.msg.sms.persistence.teacher.entity.TeacherJpaEntity
 
 @Repository
 interface HomeroomTeacherJpaRepository : CrudRepository<HomeroomTeacherJpaEntity, Long> {
     fun existsByGradeAndClassNum(grade: Int, classNum: Int): Boolean
+    fun findByTeacher(teacherJpaEntity: TeacherJpaEntity): HomeroomTeacherJpaEntity
 }
