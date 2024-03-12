6dfca0357e5440c3db3317b499c08f4ae0ee7db7
enbraining <me@xolving.com>
2024-03-13 00:10:52 +0900

    🔥 사용되지 않는 임포트 제거

--- sms-core/src/main/kotlin/team/msg/sms/domain/authentication/usecase/QueryMyAuthenticationUseCase.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/authentication/usecase/QueryMyAuthenticationUseCase.kt
@@ -2,20 +2,12 @@ package team.msg.sms.domain.authentication.usecase
 
 import org.springframework.transaction.annotation.Transactional
 import team.msg.sms.common.annotation.UseCase
-import team.msg.sms.domain.auth.model.Role
-import team.msg.sms.domain.authentication.dto.req.FiltersRequestData
 import team.msg.sms.domain.authentication.dto.res.QueryMyAuthenticationListResponseData
-import team.msg.sms.domain.authentication.dto.res.QueryRequestedAuthenticationListResponseData
 import team.msg.sms.domain.authentication.dto.res.RequestMyAuthenticationResponseData
-import team.msg.sms.domain.authentication.dto.res.RequestedAuthenticationResponseData
-import team.msg.sms.domain.authentication.exception.PermissionRoleDeniedException
 import team.msg.sms.domain.authentication.service.AuthenticationHistoryService
 import team.msg.sms.domain.authentication.service.AuthenticationService
 import team.msg.sms.domain.student.service.StudentService
-import team.msg.sms.domain.teacher.service.HomeroomTeacherService
 import team.msg.sms.domain.user.service.UserService
-import team.msg.sms.domain.authentication.model.Authentication.AuthenticationWithPageInfo as AuthenticationWithPageInfo
-import team.msg.sms.domain.authentication.model.Authentication.AuthenticationWithStudentInfoAndRequestedTime as AuthenticationWithStudentInfoAndRequestedTime
 
 @UseCase
 class QueryMyAuthenticationUseCase(
--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/authentication/repository/AuthenticationJpaRepository.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/authentication/repository/AuthenticationJpaRepository.kt
@@ -2,8 +2,6 @@ package team.msg.sms.persistence.authentication.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import team.msg.sms.domain.student.model.Student
-import team.msg.sms.domain.user.model.User
 import team.msg.sms.persistence.authentication.entity.AuthenticationJpaEntity
 import team.msg.sms.persistence.student.entity.StudentJpaEntity
 import java.util.*
