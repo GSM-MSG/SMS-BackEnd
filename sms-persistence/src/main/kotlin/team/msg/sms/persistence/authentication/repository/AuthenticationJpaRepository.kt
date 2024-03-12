1c9301a58bfd1f1b3650abe97c70463d78a6987f
enbraining <me@xolving.com>
2024-03-13 00:08:15 +0900

    ✨ 내 활동 리스트 API Persistence

--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/authentication/repository/AuthenticationJpaRepository.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/authentication/repository/AuthenticationJpaRepository.kt
@@ -2,8 +2,13 @@ package team.msg.sms.persistence.authentication.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import team.msg.sms.domain.student.model.Student
+import team.msg.sms.domain.user.model.User
 import team.msg.sms.persistence.authentication.entity.AuthenticationJpaEntity
+import team.msg.sms.persistence.student.entity.StudentJpaEntity
 import java.util.*
 
 @Repository
-interface AuthenticationJpaRepository : JpaRepository<AuthenticationJpaEntity, UUID>
+interface AuthenticationJpaRepository : JpaRepository<AuthenticationJpaEntity, UUID> {
+	fun findByStudent(studentJpaEntity: StudentJpaEntity): List<AuthenticationJpaEntity>
+}
