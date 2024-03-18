41c0991d4051231af6f686fc09a050455a533a94
enbraining <me@xolving.com>
2024-03-18 21:06:57 +0900

    ✨ 요청된 활동 승인 API Web Exception

--- sms-core/src/main/kotlin/team/msg/sms/domain/authentication/exception/error/AuthenticationErrorCode.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/authentication/exception/error/AuthenticationErrorCode.kt
@@ -12,7 +12,9 @@ enum class AuthenticationErrorCode(
     ALREADY_AWARDED_SCORE(ErrorStatus.CONFLICT, "활동에 이미 점수가 부여된 상태입니다."),
     PERMISSION_ROLE_DENIED(ErrorStatus.FORBIDDEN, "학생이거나 교장, 교감, 부장, 담임 선생님만 조회할 수 있습니다."),
     ONLY_ACCESS_MYSELF(ErrorStatus.FORBIDDEN, "자기 자신만 조회할 수 있습니다."),
-    INVALID_GRADE_CLASS(ErrorStatus.FORBIDDEN, "담임 선생님만 조회할 수 있습니다.")
+    INVALID_GRADE_CLASS(ErrorStatus.FORBIDDEN, "담임 선생님만 조회할 수 있습니다."),
+    ALREADY_GIVEN_SCORE(ErrorStatus.CONFLICT, "이미 활동에 점수가 부여되었습니다."),
+    NO_REQUESTED_ACTIVITY(ErrorStatus.CONFLICT, "활동이 아직 요청되지 않았습니다.")
     ;
 
     override fun status(): Int = status
