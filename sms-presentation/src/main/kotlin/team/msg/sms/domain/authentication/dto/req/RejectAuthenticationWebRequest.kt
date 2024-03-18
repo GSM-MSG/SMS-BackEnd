5e770efc873ee8d2ea85c0a106fa2b6993774977
enbraining <me@xolving.com>
2024-03-18 23:18:58 +0900

    ✨ 요청된 활동 반려 API Adapter Web DTO

---
+++ sms-presentation/src/main/kotlin/team/msg/sms/domain/authentication/dto/req/RejectAuthenticationWebRequest.kt
@@ -0,0 +1,16 @@
+package team.msg.sms.domain.authentication.dto.req
+
+import javax.validation.constraints.NotBlank
+
+data class RejectAuthenticationWebRequest (
+    @field:NotBlank
+    val score: Int,
+
+    @field:NotBlank
+    val reason: String,
+) {
+    fun toData() = ApproveAuthenticationRequestData(
+        score = score,
+        reason = reason
+    )
+}
