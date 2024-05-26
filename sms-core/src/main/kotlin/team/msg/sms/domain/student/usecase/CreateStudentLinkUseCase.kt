897e9b617e68614e6e43a9c69d7ee8b385f2c836
enbraining <me@xolving.com>
2024-05-26 10:51:49 +0900

    :sparkles: 학생정보 조회 링크 Usecase

---
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/usecase/CreateStudentLinkUseCase.kt
@@ -0,0 +1,32 @@
+package team.msg.sms.domain.student.usecase
+
+import team.msg.sms.common.annotation.UseCase
+import team.msg.sms.common.util.PasswordUtil.generateSecret
+import team.msg.sms.domain.student.dto.req.CreateStudentLinkRequestData
+import team.msg.sms.domain.student.dto.res.CreateStudentLinkResponseData
+import team.msg.sms.domain.student.model.StudentLink
+import team.msg.sms.domain.student.service.StudentLinkService
+import team.msg.sms.domain.student.service.StudentService
+import java.util.*
+
+@UseCase
+class CreateStudentLinkUseCase (
+	private val studentService: StudentService,
+	private val studentLinkService: StudentLinkService,
+) {
+	fun execute(createStudentLinkData: CreateStudentLinkRequestData): CreateStudentLinkResponseData {
+		val student = studentService.getStudentById(UUID.fromString(createStudentLinkData.studentId))
+		val token = generateSecret(32)
+
+		val studentLink = StudentLink(
+			token = token,
+			studentId = student.id,
+			timeToLive = createStudentLinkData.periodDay * 86400
+		)
+		studentLinkService.save(studentLink)
+
+		return CreateStudentLinkResponseData(
+			token = token
+		)
+	}
+}
