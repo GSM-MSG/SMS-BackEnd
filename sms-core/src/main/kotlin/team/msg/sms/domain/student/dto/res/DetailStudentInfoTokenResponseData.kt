76ef60d39ae79c9fdf5292a57222cfde64776941
ani2689 <ojno7777@gmail.com>
2024-05-28 10:55:32 +0900

    create :: student info data

---
+++ sms-core/src/main/kotlin/team/msg/sms/domain/student/dto/res/DetailStudentInfoTokenResponseData.kt
@@ -0,0 +1,31 @@
+package team.msg.sms.domain.student.dto.res
+
+import team.msg.sms.domain.languagecertificate.model.LanguageCertificate
+import team.msg.sms.domain.prize.dto.res.PrizeResponseData
+import team.msg.sms.domain.project.dto.res.ProjectResponseData
+import team.msg.sms.domain.student.model.Department
+import team.msg.sms.domain.student.model.FormOfEmployment
+import team.msg.sms.domain.student.model.MilitaryService
+
+data class DetailStudentInfoTokenResponseData(
+    val name: String,
+    val introduce: String,
+    val portfolioUrl: String?,
+    val grade: Int,
+    val classNum: Int,
+    val number: Int,
+    val department: Department,
+    val major: String,
+    val profileImg: String,
+    val contactEmail: String,
+    val gsmAuthenticationScore: Int,
+    val formOfEmployment: FormOfEmployment,
+    val regions: List<String>, // 근무지역
+    val militaryService: MilitaryService,
+    val salary: Int,
+    val languageCertificates: List<LanguageCertificate.LanguageCertificateScore>,
+    val certificates: List<String>,
+    val techStacks: List<String>,
+    val projects: List<ProjectResponseData>,
+    val prizes: List<PrizeResponseData>
+)
---
+++ sms-presentation/src/main/kotlin/team/msg/sms/domain/student/dto/res/DetailStudentInfoTokenWebResponse.kt
@@ -0,0 +1,33 @@
+package team.msg.sms.domain.student.dto.res
+
+import team.msg.sms.domain.languagecertificate.model.LanguageCertificate
+import team.msg.sms.domain.prize.dto.res.PrizeResponseData
+import team.msg.sms.domain.project.dto.res.ProjectResponseData
+import team.msg.sms.domain.student.model.Department
+import team.msg.sms.domain.student.model.FormOfEmployment
+import team.msg.sms.domain.student.model.MilitaryService
+
+data class DetailStudentInfoTokenWebResponse(
+    val name: String,
+    val introduce: String,
+    val portfolioUrl: String?,
+    val grade: Int,
+    val classNum: Int,
+    val number: Int,
+    val department: Department,
+    val major: String,
+    val profileImgUrl: String,
+    @Deprecated(message = "전 Beta Version 사용자들의 접근성을 개방하기 위하여 현재버전에서는 사용하지 않습니다.")
+    val profileImg: String,
+    val contactEmail: String,
+    val gsmAuthenticationScore: Int,
+    val formOfEmployment: FormOfEmployment,
+    val regions: List<String>, // 근무지역
+    val militaryService: MilitaryService,
+    val salary: Int,
+    val languageCertificates: List<LanguageCertificate.LanguageCertificateScore>,
+    val certificates: List<String>,
+    val techStacks: List<String>,
+    val projects: List<ProjectResponseData>,
+    val prizes: List<PrizeResponseData>
+)
