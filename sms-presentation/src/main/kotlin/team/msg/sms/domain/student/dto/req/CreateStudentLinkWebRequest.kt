9b6e68e6964ca58a3553972a395392a92563557b
enbraining <me@xolving.com>
2024-05-28 16:27:31 +0900

    :recycle: 링크 최소 최대 만료기간 지정

--- sms-presentation/src/main/kotlin/team/msg/sms/domain/student/dto/req/CreateStudentLinkWebRequest.kt
+++ sms-presentation/src/main/kotlin/team/msg/sms/domain/student/dto/req/CreateStudentLinkWebRequest.kt
@@ -1,9 +1,15 @@
 package team.msg.sms.domain.student.dto.req
 
 import java.util.UUID
+import javax.validation.constraints.*
 
 data class CreateStudentLinkWebRequest (
+	@field:NotBlank
 	val studentId: UUID,
+
+	@field:Min(1)
+	@field:Max(30)
+	@field:NotBlank
 	val periodDay: Long
 ) {
 	fun toData(): CreateStudentLinkRequestData =
