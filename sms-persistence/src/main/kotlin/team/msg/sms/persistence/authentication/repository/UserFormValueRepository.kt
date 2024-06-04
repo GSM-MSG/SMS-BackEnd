673973174ef33670e9bd4a14124f737d6e2472f7
huuuunee <s21024@gsm.hs.kr>
2024-06-05 07:00:31 +0900

    add: [UserFormValue] 리포지토리 + 어댑터 추가

---
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/authentication/repository/UserFormValueRepository.kt
@@ -0,0 +1,10 @@
+package team.msg.sms.persistence.authentication.repository
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import team.msg.sms.persistence.authentication.entity.UserFormValueJpaEntity
+import java.util.UUID
+
+@Repository
+interface UserFormValueRepository : CrudRepository<UserFormValueJpaEntity, UUID> {
+}
