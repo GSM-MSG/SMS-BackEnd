29d961ca45cf25a848c159cb3cf17fae27093851
huuuunee <s21024@gsm.hs.kr>
2024-06-08 03:02:45 +0900

    add: [GroupAuthenticationArea] save 함수 추가 및 구현

--- sms-persistence/src/main/kotlin/team/msg/sms/persistence/authentication/GroupAuthenticationAreaPersistenceAdapter.kt
+++ sms-persistence/src/main/kotlin/team/msg/sms/persistence/authentication/GroupAuthenticationAreaPersistenceAdapter.kt
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component
 import team.msg.sms.domain.authentication.model.GroupAuthenticationArea
 import team.msg.sms.domain.authentication.spi.GroupAuthenticationAreaPort
 import team.msg.sms.persistence.authentication.mapper.toDomain
+import team.msg.sms.persistence.authentication.mapper.toEntity
 import team.msg.sms.persistence.authentication.repository.GroupAuthenticationAreaRepository
 
 @Component
@@ -13,4 +14,7 @@ class GroupAuthenticationAreaPersistenceAdapter(
 ) : GroupAuthenticationAreaPort {
     override fun queryGroupAuthenticationArea(): List<GroupAuthenticationArea> =
         groupAuthenticationAreaRepository.findAll().sortedBy { it.sort }.map { it.toDomain() }
+
+    override fun save(groupAuthenticationArea: GroupAuthenticationArea): GroupAuthenticationArea =
+        groupAuthenticationAreaRepository.save(groupAuthenticationArea.toEntity()).toDomain()
 }
