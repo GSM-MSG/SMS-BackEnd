e370322f5e779da1735c80ec44ca0601ba1b196f
huuuunee <s21024@gsm.hs.kr>
2024-06-14 12:28:21 +0900

    add: 폼 생성 UseCase 개발

--- sms-core/src/main/kotlin/team/msg/sms/domain/authentication/usecase/CreateAuthenticationFormUseCase.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/authentication/usecase/CreateAuthenticationFormUseCase.kt
@@ -1,79 +1,166 @@
 package team.msg.sms.domain.authentication.usecase
 
 import team.msg.sms.common.annotation.UseCase
+import team.msg.sms.domain.authentication.dto.req.CreateAuthenticationAreaFormRequestData
+import team.msg.sms.domain.authentication.dto.req.CreateAuthenticationFieldRequestData
 import team.msg.sms.domain.authentication.dto.req.CreateAuthenticationFormRequestData
+import team.msg.sms.domain.authentication.dto.req.CreateAuthenticationSectionRequestData
 import team.msg.sms.domain.authentication.model.*
-import team.msg.sms.domain.authentication.service.AuthenticationFormService
-import team.msg.sms.domain.authentication.service.AuthenticationSectionService
-import team.msg.sms.domain.authentication.service.GroupAuthenticationAreaService
-import team.msg.sms.domain.authentication.service.SelectorSectionValueService
+import team.msg.sms.domain.authentication.service.*
+import team.msg.sms.domain.file.dto.req.CreateFileRequestData
+import team.msg.sms.domain.file.model.File
+import team.msg.sms.domain.file.model.FileType
+import team.msg.sms.domain.file.service.FileService
+import team.msg.sms.domain.teacher.model.Teacher
 import team.msg.sms.domain.teacher.service.TeacherService
 import java.time.LocalDateTime
 import java.util.*
 
 @UseCase
 class CreateAuthenticationFormUseCase(
-    private val groupAuthenticationAreaService: GroupAuthenticationAreaService,
+    private val authenticationAreaService: AuthenticationAreaService,
     private val authenticationFormService: AuthenticationFormService,
     private val authenticationSectionService: AuthenticationSectionService,
+    private val authenticationFieldService: AuthenticationFieldService,
     private val selectorSectionValueService: SelectorSectionValueService,
+    private val fileService: FileService,
     private val teacherService: TeacherService
 ) {
-    fun execute(createAuthenticationFromRequestData: CreateAuthenticationFormRequestData) {
+    fun execute(createAuthenticationFormRequestData: CreateAuthenticationFormRequestData) {
         val teacher = teacherService.currentTeacher()
+        val authenticationForm = saveAuthenticationForm(createAuthenticationFormRequestData, teacher)
 
-        val authenticationForm = authenticationFormService.save(
+        saveFiles(createAuthenticationFormRequestData.files, authenticationForm.id)
+        createAuthenticationAreas(createAuthenticationFormRequestData.formData, authenticationForm.id)
+    }
+
+    private fun saveAuthenticationForm(
+        createAuthenticationFormRequestData: CreateAuthenticationFormRequestData,
+        teacher: Teacher
+    ): AuthenticationForm {
+        return authenticationFormService.save(
             AuthenticationForm(
                 id = UUID.randomUUID(),
-                title = createAuthenticationFromRequestData.title,
-                version = createAuthenticationFromRequestData.version,
+                title = createAuthenticationFormRequestData.title,
+                version = createAuthenticationFormRequestData.version,
                 createdBy = teacher.id,
                 createdAt = LocalDateTime.now()
             )
         )
+    }
 
-        createAuthenticationFromRequestData.formData.forEachIndexed { groupIndex, groupData ->
-            groupAuthenticationAreaService.save(
-                GroupAuthenticationArea(
+    private fun saveFiles(files: List<CreateFileRequestData>, authenticationFormId: UUID) {
+        fileService.saveAll(
+            files.map {
+                File(
                     id = UUID.randomUUID(),
-                    title = groupData.title,
-                    sort = groupIndex,
-                    authenticationFormId = authenticationForm.id
+                    fileName = it.name,
+                    fileUrl = it.url,
+                    fileType = FileType.AUTHENTICATION,
+                    targetId = authenticationFormId
                 )
-            ).also { group ->
-                groupData.data.forEach { sectionData ->
-                    authenticationSectionService.save(
-                        AuthenticationSection(
-                            id = UUID.randomUUID(),
-                            groupId = group.id,
-                            sectionName = sectionData.sectionName,
-                            description = sectionData.description,
-                            placeHolder = sectionData.placeHolder,
-                            sectionType = sectionData.sectionType,
-                            maxCount = sectionData.maxCount,
-                            sectionScore = sectionData.sectionScore
-                        )
-                    ).also { section ->
-                        if (sectionData.sectionType in listOf(
-                                SectionType.SELECT,
-                                SectionType.SELECT_VALUE,
-                                SectionType.BOOLEAN
-                            )
-                        ) {
-                            sectionData.selectorSectionName.mapIndexed { selectorIndex, name ->
-                                SelectorSectionValue(
-                                    id = UUID.randomUUID(),
-                                    authenticationSectionId = section.id,
-                                    name = name,
-                                    sort = selectorIndex
-                                )
-                            }.also { selectorSectionValues ->
-                                selectorSectionValueService.saveAll(selectorSectionValues)
-                            }
-                        }
-                    }
-                }
             }
+        )
+    }
+
+    private fun createAuthenticationAreas(
+        formData: List<CreateAuthenticationAreaFormRequestData>,
+        authenticationFormId: UUID
+    ) {
+        formData.forEachIndexed { groupIndex, groupData ->
+            val group = saveAuthenticationArea(groupData, authenticationFormId, groupIndex)
+            createAuthenticationSections(groupData.data, group.id)
+        }
+    }
+
+    private fun saveAuthenticationArea(
+        groupData: CreateAuthenticationAreaFormRequestData,
+        authenticationFormId: UUID,
+        groupIndex: Int
+    ): AuthenticationArea {
+        return authenticationAreaService.save(
+            AuthenticationArea(
+                id = UUID.randomUUID(),
+                title = groupData.title,
+                sort = groupIndex,
+                authenticationFormId = authenticationFormId
+            )
+        )
+    }
+
+    private fun createAuthenticationSections(
+        sectionDataList: List<CreateAuthenticationSectionRequestData>,
+        groupId: UUID
+    ) {
+        sectionDataList.forEachIndexed { index, sectionData ->
+            val section = saveAuthenticationSection(sectionData, groupId, index)
+            createAuthenticationFields(sectionData.fieldData, section.id)
+        }
+    }
+
+    private fun saveAuthenticationSection(
+        sectionData: CreateAuthenticationSectionRequestData,
+        groupId: UUID,
+        index: Int
+    ): AuthenticationSection {
+        return authenticationSectionService.save(
+            AuthenticationSection(
+                id = UUID.randomUUID(),
+                groupId = groupId,
+                sectionName = sectionData.sectionName,
+                maxCount = sectionData.maxCount,
+                sort = index
+            )
+        )
+    }
+
+    private fun createAuthenticationFields(
+        fieldDataList: List<CreateAuthenticationFieldRequestData>,
+        sectionId: UUID
+    ) {
+        fieldDataList.forEachIndexed { sortIndex, fieldData ->
+            val field = saveAuthenticationField(fieldData, sectionId, sortIndex)
+            saveSelectorSectionValues(fieldData, field)
+        }
+    }
+
+    private fun saveAuthenticationField(
+        fieldData: CreateAuthenticationFieldRequestData,
+        sectionId: UUID,
+        index: Int
+    ): AuthenticationField {
+        return authenticationFieldService.save(
+            AuthenticationField(
+                id = UUID.randomUUID(),
+                sectionId = sectionId,
+                description = fieldData.description,
+                placeHolder = fieldData.placeHolder,
+                fieldInputType = fieldData.fieldInputType,
+                fieldScore = 0.0,
+                sort = index
+            )
+        )
+    }
+
+    private fun saveSelectorSectionValues(
+        fieldData: CreateAuthenticationFieldRequestData,
+        field: AuthenticationField
+    ) {
+        if (fieldData.fieldInputType in listOf(
+                SectionType.SELECT,
+                SectionType.SELECT_VALUE,
+                SectionType.BOOLEAN
+            )
+        ) {
+            val selectorSectionValues = fieldData.selectorSectionName.mapIndexed { selectorIndex, name ->
+                SelectorSectionValue(
+                    id = UUID.randomUUID(),
+                    authenticationFieldId = field.id,
+                    name = name,
+                    sort = selectorIndex
+                )
+            }
+            selectorSectionValueService.saveAll(selectorSectionValues)
         }
     }
 }
