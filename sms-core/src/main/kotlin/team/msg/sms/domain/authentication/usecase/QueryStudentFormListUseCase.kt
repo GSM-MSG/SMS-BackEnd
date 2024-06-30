6c03193afb1c7885b29734c23c7bc1efbd234f85
huuuunee <s21024@gsm.hs.kr>
2024-06-30 22:31:23 +0900

    fix: UseCase -> ReadOnlyUseCase로 변경

--- sms-core/src/main/kotlin/team/msg/sms/domain/authentication/usecase/QueryStudentFormListUseCase.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/authentication/usecase/QueryStudentFormListUseCase.kt
@@ -1,21 +1,18 @@
 package team.msg.sms.domain.authentication.usecase
 
-import org.springframework.transaction.annotation.Transactional
-import team.msg.sms.common.annotation.UseCase
+import team.msg.sms.common.annotation.ReadOnlyUseCase
 import team.msg.sms.domain.authentication.dto.res.UserBoardPageResponseData
-import team.msg.sms.domain.authentication.dto.res.UserBoardWithStudentInfoResponseData
 import team.msg.sms.domain.authentication.model.MarkingBoardType
 import team.msg.sms.domain.authentication.service.AuthenticationFormService
 import team.msg.sms.domain.authentication.service.MarkingBoardService
 import team.msg.sms.domain.student.service.StudentService
 
-@UseCase
+@ReadOnlyUseCase
 class QueryStudentFormListUseCase(
     private val studentService: StudentService,
     private val markingBoardService: MarkingBoardService,
     private val authenticationFormService: AuthenticationFormService
 ) {
-    @Transactional(readOnly = true)
     fun execute(page: Int, size: Int, type: List<MarkingBoardType>?): UserBoardPageResponseData {
         val studentIds = studentService.getStudentIds()
 
