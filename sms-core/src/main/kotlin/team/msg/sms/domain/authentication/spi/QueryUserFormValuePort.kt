7575b5d892bea7a721ef14bfa4b5b18550c55f14
huuuunee <s21024@gsm.hs.kr>
2024-07-02 05:16:55 +0900

    add: [UserFormValue] fieldId와 StudentId로 userFormValueList를 찾는 함수 추가

--- sms-core/src/main/kotlin/team/msg/sms/domain/authentication/spi/QueryUserFormValuePort.kt
+++ sms-core/src/main/kotlin/team/msg/sms/domain/authentication/spi/QueryUserFormValuePort.kt
@@ -1,4 +1,8 @@
 package team.msg.sms.domain.authentication.spi
 
+import team.msg.sms.domain.authentication.model.UserFormValue
+import java.util.UUID
+
 interface QueryUserFormValuePort {
+    fun queryUserFormValueListByFieldIdAndStudentId(fieldId: UUID, studentId: UUID): List<UserFormValue>
 }
