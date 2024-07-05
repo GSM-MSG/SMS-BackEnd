4208d7478100169c2d8c1cf54059378814776fe7
huuuunee <s21024@gsm.hs.kr>
2024-07-05 15:34:54 +0900

    add: 선생님이 학생 채점 기능 개발

---
+++ sms-core/src/main/kotlin/team/msg/sms/domain/authentication/usecase/GradingAuthenticationFormUseCase.kt
@@ -0,0 +1,61 @@
+package team.msg.sms.domain.authentication.usecase
+
+import team.msg.sms.common.annotation.UseCase
+import team.msg.sms.domain.authentication.dto.req.GradingRequestData
+import team.msg.sms.domain.authentication.exception.UserFormValueNotFoundException
+import team.msg.sms.domain.authentication.model.MarkingType
+import team.msg.sms.domain.authentication.model.MarkingValue
+import team.msg.sms.domain.authentication.service.MarkingBoardService
+import team.msg.sms.domain.authentication.service.MarkingValueService
+import team.msg.sms.domain.authentication.service.UserFormValueService
+import team.msg.sms.domain.teacher.service.TeacherService
+import java.time.LocalDateTime
+import java.util.UUID
+
+@UseCase
+class GradingAuthenticationFormUseCase(
+    private val markingValueService: MarkingValueService,
+    private val markingBoardService: MarkingBoardService,
+    private val userFormValueService: UserFormValueService,
+    private val teacherService: TeacherService
+) {
+    fun execute(markingBoardId: UUID, gradingDataList: List<GradingRequestData>) {
+        //요청한 setId 들 중 실제 userFormValue 테이블에 없는 setId 일 경우 최종점수에 영향을 미칠 수 있어 예외처리
+        if (!userFormValueService.checkUserFormValueBySetIds(gradingDataList.map { it.setId })) throw UserFormValueNotFoundException
+
+        val teacher = teacherService.currentTeacher()
+
+        val markingValueList = markingValueService.findMarkingValueListByMarkingBoardId(markingBoardId)
+
+        val markingValueMap = markingValueList.associateBy { it.setId }
+
+        val saveMarkingValueList = markingValueService.saveAll(
+            gradingDataList.map { gradingData ->
+                markingValueMap[gradingData.setId]?.copy(
+                    score = gradingData.score,
+                    createdAt = LocalDateTime.now(),
+                    createdBy = teacher.id,
+                    type = if (gradingData.score == null) MarkingType.REJECT else MarkingType.RESOLVE
+                ) ?: MarkingValue(
+                    id = UUID.randomUUID(),
+                    score = gradingData.score,
+                    createdAt = LocalDateTime.now(),
+                    createdBy = teacher.id,
+                    setId = gradingData.setId,
+                    markingBoardId = markingBoardId,
+                    type = if (gradingData.score == null) MarkingType.REJECT else MarkingType.RESOLVE
+                )
+            }
+        )
+
+        val totalScore = saveMarkingValueList.sumOf { it.score ?: 0.0 }
+
+        markingBoardService.getMarkingBoardById(markingBoardId).let { markingBoard ->
+            markingBoardService.save(
+                markingBoard.copy(
+                    totalScore = totalScore
+                )
+            )
+        }
+    }
+}
